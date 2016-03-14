f57c23d08245a01458559d13f30d7a452020a169
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2016-03-14 12:31:59 +0000

    restored c++ ismrmrd order of dimensions

--- examples/demo1.py
+++ examples/demo1.py
@@ -45,7 +45,7 @@ try:
     for i in range(images.number()):
         data = images.image_as_array(i)
         pylab.figure(i + 1)
-        pylab.imshow(data[0,0,:,:])
+        pylab.imshow(data[:,:,0,0])
         pylab.show()
 
     # write images to a new group in 'output1.h5'
