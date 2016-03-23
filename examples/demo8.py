ae16d0682ad28802d8e2d505aa95af690c57e56b
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2016-03-23 12:25:13 +0000

    nicer printing in demo8.py

--- examples/demo8.py
+++ examples/demo8.py
@@ -15,14 +15,13 @@ try:
     # acquisitions will be read from this HDF file
     input_data = ISMRMRDAcquisitions('testdata.h5')
 
-    print('---')
-    print('acquisition data norm:', input_data.norm())
+#    print('---')
+    print('---\n acquisition data norm: %e' % input_data.norm())
 
     # print('processing acquisitions...')
     interim_data = MR_remove_x_oversampling(input_data)
 
-    print('---')
-    print('processed acquisition data norm:', interim_data.norm())
+    print('---\n processed acquisition data norm: %e' % interim_data.norm())
 
     # perform reconstruction
     recon = SimpleReconstructionProcessor()
@@ -31,8 +30,7 @@ try:
     recon.process()
     interim_images = recon.get_output()
 
-    print('---')
-    print('reconstructed images norm:', interim_images.norm())
+    print('---\n reconstructed images norm: %e' % interim_images.norm())
 
     # post-process reconstructed images
     # print('processing images...')
@@ -45,8 +43,7 @@ try:
     # use the acquisition model (forward projection) to produce acquisitions
     acqs = am.forward(interim_images)
 
-    print('---')
-    print('their forward projection norm:', acqs.norm())
+    print('---\n their forward projection norm %e' % acqs.norm())
 
     # compute the difference between real and modelled acquisitions:
     #   diff = acqs - P acqs,
@@ -54,32 +51,31 @@ try:
     a = -acqs.dot(input_data) / input_data.dot(input_data)
     b = 1.0
     diff = AcquisitionsContainer.axpby(a, input_data, b, acqs)
-    print('---')
-    print('reconstruction residual norm (rel):', diff.norm()/acqs.norm())
+    rr = diff.norm()/acqs.norm()
+    print('---\n reconstruction residual norm (rel): %e' % rr)
 
     # apply the adjoint model (backward projection)
     imgs = am.backward(diff)
 
-    print('---')
-    print('its backward projection norm:', imgs.norm())
+    print('---\n its backward projection norm: %e' % imgs.norm())
 
     # test that the backward projection is the adjoint of forward
     # on x = diff and y = interim_images
     # (note that x = (1 - P)F y, so the result must be numerically real)
-    print('---')
-    print('(x, F y) =', diff.dot(acqs))
-    print('= (B x, y) =', imgs.dot(interim_images))
+    xFy = diff.dot(acqs)
+    print('---\n (x, F y) = %e + i%e' % (xFy.real, xFy.imag))
+    Bxy = imgs.dot(interim_images)
+    print('= (B x, y) = %e + i%e' % (Bxy.real, Bxy.imag))
 
     # test images norm
     s = imgs.norm()
-    print('---')
-    print('(B x, B x) =', imgs.dot(imgs), '=', s*s)
+    ss = imgs.dot(imgs)
+    print('---\n (B x, B x) = %e + i%e = %e' % (ss.real, ss.imag, s*s))
 
     # test linear combination of images
     a = -1.0
     im_diff = ImagesContainer.axpby(a, imgs, b, imgs)
-    print('---')
-    print('0.0 =', im_diff.norm())
+    print('---\n 0.0 = %e' % im_diff.norm())
 
     # plot obtained images
     for i in range(images.number()):
