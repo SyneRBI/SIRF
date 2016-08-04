5acbde505a367093bee43f685a644748da791d1f
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2016-08-04 16:38:24 +0100

    synchronized Gadget.m with python's Gadget

--- mGadgetron/+gadgetron/Gadget.m
+++ mGadgetron/+gadgetron/Gadget.m
@@ -4,9 +4,22 @@ classdef Gadget < handle
         name_
     end
     methods
-        function self = Gadget(name)
+        function self = Gadget(fullname)
+            i = strfind(fullname, '(');
+            if isempty(i)
+                name = strtrim(fullname);
+                prop = [];
+            else
+                j = strfind(fullname(i + 1 : end), ')');
+                name = strtrim(fullname(1 : i - 1));
+                prop = fullname(i + 1 : i + j - 1);
+            end
+            %fprintf(name, prop);
             self.handle_ = calllib('mgadgetron', 'mGT_newObject', name);
             gadgetron.checkExecutionStatus(name, self.handle_);
+            if ~isempty(prop)
+                self.set_properties(prop)
+            end
             self.name_ = name;
         end
         function delete(self)
@@ -17,6 +30,12 @@ classdef Gadget < handle
         function set_property(self, prop, value)
             handle = calllib('mgadgetron', 'mGT_setGadgetProperty', ...
                 self.handle_, prop, value);
+            gadgetron.checkExecutionStatus(self.name_, handle)
+            calllib('mutilities', 'mDeleteDataHandle', handle)
+        end
+        function set_properties(self, prop)
+            handle = calllib('mgadgetron', 'mGT_setGadgetProperties', ...
+                self.handle_, prop);
             gadgetron.checkExecutionStatus(self.name_, handle)
             calllib('mutilities', 'mDeleteDataHandle', handle)
         end
