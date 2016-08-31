9eaeaff0577cd9c7b1f70b7866309997902d0668
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2016-08-31 15:37:49 +0100

    added tests project to cSTIR

---
+++ cSTIR/tests/tests.h
@@ -0,0 +1,25 @@
+#ifndef XSTIR_TESTS
+#define XSTIR_TESTS
+
+#define CAST_REF(T, X, Y) T& X = (T&)Y
+
+#define OBJECT(Base, Obj, X, sptr_X) \
+	boost::shared_ptr< Base > sptr_X(new Obj); Obj& X = (Obj&)*sptr_X
+
+#define CATCH \
+	catch (LocalisedException& se) {\
+		ExecutionStatus status(se);\
+		DataHandle* handle = new DataHandle;\
+		handle->set(0, &status);\
+		return (void*)handle;\
+		}\
+	catch (...) {\
+		ExecutionStatus status("unhandled exception", __FILE__, __LINE__);\
+		DataHandle* handle = new DataHandle;\
+		handle->set(0, &status);\
+		return (void*)handle;\
+		}\
+
+void test1();
+
+#endif
