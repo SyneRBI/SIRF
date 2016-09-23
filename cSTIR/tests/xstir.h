57518eda18348423edc6dd6e805960c62d92bb96
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2016-09-23 14:47:34 +0100

    some more work done on PETAcquisitionModel

--- cSTIR/tests/xstir.h
+++ cSTIR/tests/xstir.h
@@ -19,6 +19,34 @@ public:
 	{
 		sptr_background_ = sptr;
 	}
+	void set_additive_term(boost::shared_ptr<ProjData> sptr)
+	{
+		sptr_add_ = sptr;
+	}
+	void set_normalisation(boost::shared_ptr<BinNormalisation> sptr)
+	{
+		sptr_normalisation_ = sptr;
+	}
+	void set_normalisation(boost::shared_ptr<ProjData> sptr)
+	{
+		sptr_normalisation_.reset(new BinNormalisationFromProjData(sptr));
+	}
+	//void set_multiplicative_term(boost::shared_ptr<ProjData> sptr)
+	//{
+	//	sptr_mult_ = sptr;
+	//}
+	void drop_background_term()
+	{
+		sptr_background_.reset();
+	}
+	void drop_additive_term()
+	{
+		sptr_add_.reset();
+	}
+	void drop_normalisation()
+	{
+		sptr_normalisation_.reset();
+	}
 	boost::shared_ptr<ProjData> forward(const Image& image)
 	{
 		boost::shared_ptr<ProjData> sptr_fd(
@@ -53,6 +81,7 @@ private:
 	boost::shared_ptr<ProjectorByBinPair> sptr_projectors_;
 	boost::shared_ptr<ProjData> sptr_acq_template_;
 	boost::shared_ptr<Image> sptr_image_template_;
+	//boost::shared_ptr<ProjData> sptr_mult_;
 	boost::shared_ptr<ProjData> sptr_add_;
 	boost::shared_ptr<ProjData> sptr_background_;
 	boost::shared_ptr<BinNormalisation> sptr_normalisation_;
