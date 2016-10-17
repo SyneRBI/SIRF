31baad02c85e96385b846428e843edcad35a56eb
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2016-10-17 16:46:04 +0100

    added command line option to c2m

--- c2m/main.cpp
+++ c2m/main.cpp
@@ -17,22 +17,29 @@ int main(int argc, char **argv)
 {
 	int status;
 	string path;
-	path = "../";
-	status = c2m\
-		("IUTILITIES", "---", path, "iutilities.h", "mutilities.h", "mutilities.c");
-	if (status)
-		cout << "wrong input file format" << endl;
 
-	path = "../../xGadgetron/cGadgetron/";
-	status = c2m\
-		("CGADGETRON", "cGT", path, "cgadgetron.h", "mgadgetron.h", "mgadgetron.c");
-	if (status)
-		cout << "wrong input file format" << endl;
+	if (argc < 2 || argv[1][0] == 'u') {
+		path = "../";
+		status = c2m\
+			("IUTILITIES", "---", path, "iutilities.h", "mutilities.h", "mutilities.c");
+		if (status)
+			cout << "wrong input file format" << endl;
+	}
 
-	path = "../../xSTIR/cSTIR/";
-	status = c2m("CSTIR", "cSTIR", path, "cstir.h", "mstir.h", "mstir.c", 1);
-	if (status)
-		cout << "wrong input file format" << endl;
+	if (argc < 2 || argv[1][0] == 'g') {
+		path = "../../xGadgetron/cGadgetron/";
+		status = c2m\
+			("CGADGETRON", "cGT", path, "cgadgetron.h", "mgadgetron.h", "mgadgetron.c");
+		if (status)
+			cout << "wrong input file format" << endl;
+	}
+
+	if (argc < 2 || argv[1][0] == 's') {
+		path = "../../xSTIR/cSTIR/";
+		status = c2m("CSTIR", "cSTIR", path, "cstir.h", "mstir.h", "mstir.c", 1);
+		if (status)
+			cout << "wrong input file format" << endl;
+	}
 
 	cout << "Press any key to continue ";
 	_getch();
