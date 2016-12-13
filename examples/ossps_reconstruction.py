cecb1bd15fc14057ca60af1c5c9050934dcd3d90
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2016-12-13 11:27:49 +0000

    tidied up ossps demo

--- examples/ossps_reconstruction.py
+++ examples/ossps_reconstruction.py
@@ -4,8 +4,15 @@ Usage:
   ossps_reconstruction [--help | options]
 
 Options:
-  -e <engn>, --engine=<engn>  reconstruction engine [default: Stir]
-  -p <path>, --path=<path>    sub-path to engine module [default: /xSTIR/pSTIR]
+  -f <file>, --file=<file>  raw data file
+                            [default: Utahscat600k_ca_seg4.hs]
+  -g <file>, --init=<file>  initial image guess file
+                            [default: test_image_PM_QP_6.hv]
+  -p <fact>, --penf=<fact>  penalty factor [default: 0]
+  -s <subs>, --subs=<subs>  number of subsets [default: 4]
+  -i <iter>, --iter=<iter>  number of iterations [default: 2]
+  --engine=<engn>  reconstruction engine [default: Stir]
+  --enpath=<path>  sub-path to engine module [default: /xSTIR/pSTIR]
 '''
 
 __version__ = '0.1.0'
@@ -14,99 +21,76 @@ args = docopt(__doc__, version=__version__)
 
 import os
 import sys
-sys.path.append(os.environ.get('SRC_PATH') + args['--path'])
+sys.path.append(os.environ.get('SRC_PATH') + args['--enpath'])
 exec('from p' + args['--engine'] + ' import *')
 
 def main():
 
-    # direct all diagnostic printing to a file
+    # direct all printing to this file
     printer = printerTo('output.txt')
 
-    # create matrix to be used by projectors
-    matrix = RayTracingMatrix()
-    matrix.set_num_tangential_LORs(2)
-    n = matrix.get_num_tangential_LORs()
+    # select acquisition model that implements the geometric
+    # forward projection by a ray tracing matrix multiplication
+    am = AcquisitionModelUsingMatrix\
+         (RayTracingMatrix().set_num_tangential_LORs(2))
 
-    # create acquisition model
-    am = AcquisitionModelUsingMatrix()
-    am.set_matrix(matrix)
+    # PET acquisition data to be read from the file specified by --file option
+    ad = AcquisitionData(args['--file'])
 
-    # read acquisition model data
-    ad = AcquisitionData('Utahscat600k_ca_seg4.hs')
-
-    # create prior
-    prior = QuadraticPrior()
-    prior.set_penalisation_factor(0.5)
-
-    # create objective function
+    # define objective function to be maximized as
+    # Poisson logarithmic likelihood with linear model for mean
     obj_fun = PoissonLogLh_LinModMean_AcqMod()
-    obj_fun.set_sensitivity_filename('RPTsens_seg3_PM.hv')
-    obj_fun.set_recompute_sensitivity(True)
-    obj_fun.set_use_subset_sensitivities(False)
-    obj_fun.set_zero_seg0_end_planes(True)
-    obj_fun.set_max_segment_num_to_process(3)
     obj_fun.set_acquisition_model(am)
     obj_fun.set_acquisition_data(ad)
-    obj_fun.set_prior(prior)
+    fact = float(args['--penf'])
+    obj_fun.set_prior(QuadraticPrior().set_penalisation_factor(fact))
 
-    # create OSSPS reconstructor
+    num_subsets = int(args['--subs'])
+    num_subiterations = int(args['--iter'])
+
+    # select Ordered Subsets Separable Paraboloidal Surrogate
+    # as the reconstruction algorithm
     recon = OSSPSReconstruction()
-    recon.set_num_subsets(4)
-    recon.set_num_subiterations(8)
-    recon.set_save_interval(8)
-    recon.set_relaxation_parameter(2)
+    recon.set_num_subsets(num_subsets)
+    recon.set_num_subiterations(num_subiterations)
     recon.set_objective_function(obj_fun)
 
-    # read an initial estimate for the reconstructed image from a file
-    image = Image()
-    image.read_from_file('test_image_PM_QP_6.hv')
+    # read an initial estimate for the reconstructed image from the file
+    # specified by --init option
+    image = Image(args['--init'])
 
     # set up the reconstructor
     print('setting up, please wait...')
     recon.set_up(image)
 
-    # obtain the subiterations range
-    start = recon.get_start_subiteration_num()
-    stop = recon.get_num_subiterations()
-    print('subiteration range: %d to %d' % (start, stop))
-
     # in order to see the reconstructed image evolution
     # take over the control of the iterative process
     # rather than allow recon.reconstruct to do all job at once
-
-    # not needed (set by set_up), just to show we can start at any iteration 
-    recon.set_subiteration_num(start)
-    for iter in range(start, stop + 1):
+    for iter in range(num_subiterations):
         print('\n------------- Subiteration %d' % recon.get_subiteration_num())
         # perform an iteration
         recon.update(image)
         # plot the current image
         data = image.as_array()
-        pylab.figure(iter)
+        pylab.figure(iter + 1)
         pylab.imshow(data[10,:,:])
         print('close Figure %d window to continue' % iter)
         pylab.show()
-
-    # compare the reconstructed image to the expected image
-    expectedImage = Image()
-    expectedImage.read_from_file('test_image_OSSPS_PM_QP_8.hv')
-    diff = expectedImage.diff_from(image)
-    print('difference from expected image: %e' % diff)
 
     # let the user inspect any z-crossections of the image they want to
     data = image.as_array()
     nz = data.shape[0]
     print('Enter z-coordinate of the slice to view it')
-    print('(a value outside the range [0 : %d] will stop the loop)'%(nz - 1))
+    print('(a value outside the range [1 : %d] will stop the loop)'%nz)
     while True:
         s = str(input('z-coordinate: '))
         if len(s) < 1:
             break
         z = int(s)
-        if z < 0 or z >= nz:
+        if z < 1 or z > nz:
             break
         pylab.figure(z)
-        pylab.imshow(data[z,:,:])
+        pylab.imshow(data[z - 1,:,:])
         print('close Figure %d window to continue' % z)
         pylab.show()
 
