c1bcab3b823ef5d1d4ec289113a0287cff269b5d
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2016-12-16 06:22:50 +0000

    additional comments to MR recon demos

--- examples/coil_sensitivity_maps.py
+++ examples/coil_sensitivity_maps.py
@@ -1,5 +1,6 @@
 '''
-Demo for CSMs computation from coil images by xGadgetron and ismrmrd-python-tools
+Medium-level demo demonstrating how 2D coil sensitivity maps can be obtained 
+from a multi-coil 2D Cartesian MR acquisition
 '''
 
 import argparse
@@ -11,20 +12,17 @@ import time
 
 BUILD_PATH = os.environ.get('BUILD_PATH') + '/xGadgetron'
 SRC_PATH = os.environ.get('SRC_PATH') + '/xGadgetron/pGadgetron'
-DATA_PATH = os.environ.get('SRC_PATH') + '/xGadgetron/examples/'
-IPT_PATH = os.environ.get('SRC_PATH') + '/ismrmrd-python-tools'
 
 sys.path.append(BUILD_PATH)
 sys.path.append(SRC_PATH)
-sys.path.append(IPT_PATH)
-
-from ismrmrdtools import coils
 
 from pGadgetron import *
+from ismrmrdtools import coils
 
 parser = argparse.ArgumentParser(description = \
 '''
-Demo for CSMs computation from coil images by xGadgetron and ismrmrd-python-tools
+Medium-level demo demonstrating how 2D coil sensitivity maps can be obtained 
+from a multi-coil 2D Cartesian MR acquisition
 ''')
 parser.add_argument\
 ('filename', nargs='?', default = 'simulated_MR_2D_cartesian.h5', \
@@ -46,65 +44,40 @@ def show(image_matrix, tile_shape, scale, titles):
     print('close figure 1 to continue')
     plt.show()
 
-try:
- 
-    input_data = AcquisitionData(DATA_PATH + args.filename)
-    prep_gadgets = ['RemoveROOversamplingGadget']
-    processed_data = input_data.process(prep_gadgets)
-    print('sorting acquisitions...')
-    processed_data.sort()
 
+def main():
+    # acquisitions will be read from an HDF file args.filename
+    input_data = AcquisitionData(args.filename)
+    
+    # pre-process acquired k-space data
+    processed_data = MR_remove_x_oversampling(input_data)
+    
+    # sort k-space data into a 2D Cartesian matrix for each coil
+    processed_data.sort()
+    
+    # create object containing images for each coil
     CIs = CoilImages()
-    print('computing coil images...')
     CIs.calculate(processed_data)
 
+    # create coil sensitivity object
     CSMs = CoilSensitivityMaps()
-    print('computing sensitivity maps...')
+
+    # calculate coils using 
     CSMs.calculate(CIs, method = 'SRSS(niter = 10)')
 
-    nz = CSMs.number()
-    print('%d slices' % nz)
 
-    while True:
-        print('---\n Enter the slice number to view it.')
-        print(' A value outside the range [1 : %d] will stop this loop.'% nz)
-        s = str(input('slice: '))
-        if len(s) < 1:
-            break
-        z = int(s) - 1
-        if z < 0 or z >= nz:
-            break
-
-        coil_images = numpy.squeeze(CIs.as_array(z))
-
-        print('computing sensitivity maps (Inati)...')
-        (csm_inati, rho) = coils.calculate_csm_inati_iter\
-                           (coil_images, niter = 10)
-
-        csm_srss = numpy.squeeze(CSMs.as_array(z))
-        nc, ny, nx = csm_srss.shape
-        images = numpy.ndarray((2, ny, nx), dtype = numpy.float64)
-        images[0, :, :] = abs(numpy.sum(csm_srss * coil_images, axis = 0))
-        images[1, :, :] = abs(numpy.sum(csm_inati * coil_images, axis = 0))
-        maxv = numpy.amax(images)
-        show(images, tile_shape = (1,2), scale = (0, maxv),\
-            titles = ['Square Root of the Sum of Squares', 'Inati'])
-
-        csms = numpy.ndarray((2*nc, ny, nx), dtype = numpy.float64)
-        csms[ 0 :   nc, :, :] = abs(csm_srss)
-        csms[nc : 2*nc, :, :] = abs(csm_inati)
-        t = ['SRSS']
-        t.extend(['' for x in range(nc - 1)])
-        t.extend(['Inati'])
-        t.extend(['' for x in range(nc - 1)])
-        show(csms, tile_shape = (4,4), scale = (0,1), titles = t)
-
-        print('computing sensitivity maps (Walsh)...')
-        (csm_walsh, rho) = coils.calculate_csm_walsh(coil_images)
-        csms[nc : 2*nc, :, :] = abs(csm_walsh)
-        t[8] = 'Walsh'
-        show(csms, tile_shape = (4,4), scale = (0,1), titles = t)
+    # display coil sensitivity maps
+    coil_images = numpy.squeeze(CSMs.as_array(0))
+    maxv = numpy.amax(coil_images)
+    show(abs(coil_images[0::2,:,:]), tile_shape = (1,4), scale = (0, maxv),\
+        titles = ['Abs(Coil1)', 'Abs(Coil3)','Abs(Coil5)','Abs(Coil7)'])
+    show(numpy.angle(coil_images[0::2,:,:]), tile_shape = (1,4), scale = (0, maxv),\
+        titles = ['Angle(Coil1)', 'Angle(Coil3)','Angle(Coil5)','Angle(Coil7)'])    
+    
+try:
+    main()
+    print('done')
 
 except error as err:
     # display error information
-    print ('Gadgetron exception occured:\n', err.value)
+    print('??? %s' % err.value)
--- examples/fully_sampled_recon_three_chains.py
+++ examples/fully_sampled_recon_three_chains.py
@@ -1,14 +1,17 @@
 '''
-Lower-level interface demo that illustrates creating and running gadget chains
+Medium-level interface demo that illustrates 2D Cartesian MR image 
+reconstruction using Gadgetron by creating and running multiple gadget chains
 of 3 types:
 - acquisition processing chain
 - reconstruction chain
 - image processing chain
+and how to visualise or modify data in between these chains.
 '''
 
 import argparse
 import os
 import sys
+import matplotlib.pyplot as plt
 
 BUILD_PATH = os.environ.get('BUILD_PATH') + '/xGadgetron'
 SRC_PATH = os.environ.get('SRC_PATH') + '/xGadgetron/pGadgetron'
@@ -20,41 +23,69 @@ from pGadgetron import *
 
 parser = argparse.ArgumentParser(description = \
 '''
-Lower-level interface demo that illustrates creating and running gadget chains
+Medium-level interface demo that illustrates 2D Cartesian MR image 
+reconstruction using Gadgetron by creating and running multiple gadget chains
 of 3 types:
-(i) acquisition processing chain
-(ii) reconstruction chain
-(iii) image processing chain
+- acquisition processing chain
+- reconstruction chain
+- image processing chain
+and how to visualise or modify data in between these chains.
 ''')
 parser.add_argument\
 ('filename', nargs='?', default = 'simulated_MR_2D_cartesian.h5', \
  help = 'raw data file name (default: simulated_MR_2D_cartesian.h5)')
 args = parser.parse_args()                                 
 
+def gaussian(x, mu, sig):
+    return numpy.exp(-numpy.power(x - mu, 2.) / (2 * numpy.power(sig, 2.)))
+    
 def main():
 
     # acquisitions will be read from an HDF file args.filename
     input_data = AcquisitionData(args.filename)
-
+    
+    # Get size of current k-space data
+    kdim = input_data.slice_dimensions()
+    print "Size of k-space reduced from", kdim,
+    
     # pre-process acquisitions
+    # Create an object which removes the readout oversampling from the acquired 
+    # k-space data
     acq_proc = AcquisitionsProcessor(['RemoveROOversamplingGadget'])
-    print('processing acquisitions...')
     preprocessed_data = acq_proc.process(input_data)
+    
+    # Get size of k-space after removal of oversampling
+    kdim = preprocessed_data.slice_dimensions()
+    print "to", kdim, "."
+    
+    # Create simple Gaussian weighting function and apply it along the
+    # readout direction onto the k-space data
+    print "Apply Gaussian weighting function along readout"
+    gauss_weight = gaussian(numpy.array([numpy.linspace(-kdim[0]/2, kdim[0]/2, kdim[0])]),0,20)
+    gauss_weight = numpy.tile(gauss_weight.T, (1,kdim[1]))
+    dat_array = preprocessed_data.as_array()
+    for c in range(kdim[2]):
+        dat_array[:,:,c] = numpy.multiply(dat_array[:,:,c], gauss_weight)
+        
 
     # create reconstruction object
-    recon = ImagesReconstructor\
-        (['SimpleReconGadgetSet(trigger_dimension=repetition,split_slices=true)'])
-    # connect to preprocessed data
+    recon = ImagesReconstructor(['AcquisitionAccumulateTriggerGadget(trigger_dimension=repetition)', \
+        'BucketToBufferGadget(split_slices=true, verbose=false)', 
+        'SimpleReconGadget', 'ImageArraySplitGadget'])
+    
+    # provide pre-processed k-space data
     recon.set_input(preprocessed_data)
+    
     # perform reconstruction
-    print('reconstructing...')
     recon.process()
-    # get reconstructed images
+    
+    # retrieve reconstructed images
     complex_images = recon.get_output()
 
     # post-process reconstructed images
+    # Rather than using the function get_output(), a new object based on the 
+    # gadget "ExtractGadget" is specified to retrieve the image data
     img_proc = ImagesProcessor(['ExtractGadget'])
-    print('processing images...')
     images = img_proc.process(complex_images)
 
     # show obtained images
