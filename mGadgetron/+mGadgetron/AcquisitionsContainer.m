1cb2392ca20a7d1f447093274828649fe2d8d71a
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2016-12-22 15:25:17 +0000

    increased potential number of acquisition data dimensions to 16 (currently 4 in use)

--- mGadgetron/+mGadgetron/AcquisitionsContainer.m
+++ mGadgetron/+mGadgetron/AcquisitionsContainer.m
@@ -28,29 +28,35 @@ classdef AcquisitionsContainer < mGadgetron.DataContainer
             a = ap.process(self);
         end
         function [ns, ny, nc] = slice_dimensions(self)
-            ptr_i = libpointer('int32Ptr', zeros(3, 1));
+            ptr_i = libpointer('int32Ptr', ones(16, 1));
             calllib...
                 ('mgadgetron', 'mGT_getAcquisitionsDimensions', ...
                 self.handle_, ptr_i);
             dim = ptr_i.Value;
             ns = dim(1);
-            ny = dim(2);
-            nc = dim(3);
+            nc = dim(2);
+            ny = dim(3);
         end
         function data = slice_as_array(self, num)
-            ptr_i = libpointer('int32Ptr', zeros(3, 1));
+            ptr_i = libpointer('int32Ptr', ones(16, 1));
             calllib...
                 ('mgadgetron', 'mGT_getAcquisitionsDimensions', ...
                 self.handle_, ptr_i);
             dim = ptr_i.Value;
-            n = dim(1)*dim(2)*dim(3);
+            ns = dim(1);
+            nc = dim(2);
+            ny = dim(3);
+            n = ns*nc*ny;
+            %n = dim(1)*dim(2)*dim(3);
             ptr_re = libpointer('doublePtr', zeros(n, 1));
             ptr_im = libpointer('doublePtr', zeros(n, 1));
             calllib...
                 ('mgadgetron', 'mGT_getAcquisitionsData', ...
                 self.handle_, num - 1, ptr_re, ptr_im);
-            re = reshape(ptr_re.Value, dim(1), dim(2), dim(3));
-            im = reshape(ptr_im.Value, dim(1), dim(2), dim(3));
+            re = reshape(ptr_re.Value, ns, ny, nc);
+            im = reshape(ptr_im.Value, ns, ny, nc);
+%             re = reshape(ptr_re.Value, dim(1), dim(2), dim(3));
+%             im = reshape(ptr_im.Value, dim(1), dim(2), dim(3));
             data = re + 1i*im;
         end
     end
