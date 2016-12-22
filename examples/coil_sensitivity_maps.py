1cec64ca7547b35dddcca48d480c8a0fc43929bc
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2016-12-22 20:22:28 +0000

    csm + acq handling commented

--- examples/coil_sensitivity_maps.py
+++ examples/coil_sensitivity_maps.py
@@ -62,18 +62,50 @@ def main():
     # create coil sensitivity object
     CSMs = CoilSensitivityMaps()
 
-    # calculate coils using 
+    # calculate coil sensitivity maps by dividing each coil by the
+    # root-sum-of-squares over all coils (SRSS)
+    # (niter = 10) applies an iterative smoothing algorithm with 10 iterations 
+    # to the image data prior to the caluclation of the coil sensitivity maps
     CSMs.calculate(CIs, method = 'SRSS(niter = 10)')
 
 
     # display coil sensitivity maps
     coil_images = numpy.squeeze(CSMs.as_array(0))
-    maxv = numpy.amax(coil_images)
+    maxv = numpy.amax(abs(coil_images))
     show(abs(coil_images[0::2,:,:]), tile_shape = (1,4), scale = (0, maxv),\
         titles = ['Abs(Coil1)', 'Abs(Coil3)','Abs(Coil5)','Abs(Coil7)'])
     show(numpy.angle(coil_images[0::2,:,:]), tile_shape = (1,4), scale = (0, maxv),\
-        titles = ['Angle(Coil1)', 'Angle(Coil3)','Angle(Coil5)','Angle(Coil7)'])    
+        titles = ['Angle(Coil1)', 'Angle(Coil3)','Angle(Coil5)','Angle(Coil7)']) 
+        
+        
+    # calculate coil sensitivity maps directly from the raw k-space data 
+    # so far no additional parameters can be set for this method such as the
+    # number of smoothing iterations which leads to noisier coil sensitivity 
+    # maps    
+    CSMs = CoilSensitivityMaps()    
+    CSMs.calculate(processed_data)
     
+    # display coil sensitivity maps
+    coil_images = numpy.squeeze(CSMs.as_array(0))
+    maxv = numpy.amax(abs(coil_images))
+    show(abs(coil_images[0::2,:,:]), tile_shape = (1,4), scale = (0, maxv),\
+        titles = ['Abs(Coil1)', 'Abs(Coil3)','Abs(Coil5)','Abs(Coil7)'])
+      
+      
+    # calculate coil sensitivity maps using an approach suggested by 
+    #   Inati SJ, Hansen MS, Kellman P.
+    #   A solution to the phase problem in adaptive coil combination.
+    #   In: ISMRM proceeding; April; Salt Lake City, Utah, USA; 2013. 2672.  
+    # for more details please see 
+    # gadgetron/toolboxes/mri_core/mri_core_coil_map_estimation.h  
+    CSMs = CoilSensitivityMaps()  
+    CSMs.calculate(CIs, method = 'Inati()')
+        
+    # display coil sensitivity maps
+    coil_images = numpy.squeeze(CSMs.as_array(0))
+    maxv = numpy.amax(abs(coil_images))
+    show(abs(coil_images[0::2,:,:]), tile_shape = (1,4), scale = (0, maxv),\
+        titles = ['Abs(Coil1)', 'Abs(Coil3)','Abs(Coil5)','Abs(Coil7)'])
 try:
     main()
     print('done')
