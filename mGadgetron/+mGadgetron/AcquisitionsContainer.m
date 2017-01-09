00cb3b144329b7fec3d163decb3dc4418ae98ca0
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2017-01-09 14:27:36 +0000

    implemented as_array() and fill() methods for matlab acquisition data

--- mGadgetron/+mGadgetron/AcquisitionsContainer.m
+++ mGadgetron/+mGadgetron/AcquisitionsContainer.m
@@ -27,6 +27,57 @@ classdef AcquisitionsContainer < mGadgetron.DataContainer
             ap = mGadgetron.AcquisitionsProcessor(list);
             a = ap.process(self);
         end
+        function [ns, nc, na] = dimensions(self, select)
+            ptr_i = libpointer('int32Ptr', ones(16, 1));
+            calllib...
+                ('mgadgetron', 'mGT_getAcquisitionsDimensions', ...
+                self.handle_, ptr_i);
+            dim = ptr_i.Value;
+            all = true;
+            if nargin > 1
+                all = strcmp(select, 'all');
+            end
+            ns = dim(1);
+            nc = dim(2);
+            if all
+                na = self.number();
+            else
+                na = prod(dim(3:end));
+            end
+        end
+        function data = as_array(self, select)
+            if nargin < 2
+                select = 'all';
+            end
+            [ns, nc, ma] = self.dimensions(select);
+            na = self.number();
+            if strcmp(select, 'all')
+                n = na;
+                ma = na;
+            else
+                n = na + 1;
+            end
+            m = ns*nc*ma;
+            ptr_re = libpointer('doublePtr', zeros(m, 1));
+            ptr_im = libpointer('doublePtr', zeros(m, 1));
+            calllib...
+                ('mgadgetron', 'mGT_getAcquisitionsData', ...
+                self.handle_, n, ptr_re, ptr_im);
+            re = reshape(ptr_re.Value, ns, nc, ma);
+            im = reshape(ptr_im.Value, ns, nc, ma);
+            data = re + 1i*im;
+        end
+        function fill(self, data)
+            [ns, nc, na] = size(data);
+            re = real(data);
+            im = imag(data);
+            ptr_re = libpointer('doublePtr', re);
+            ptr_im = libpointer('doublePtr', im);
+            h = calllib('mgadgetron', 'mGT_setAcquisitionsData', ...
+                self.handle_, na, nc, ns, ptr_re, ptr_im);
+            mGadgetron.checkExecutionStatus('AcquisitionsContainer', h);
+            calllib('mutilities', 'mDeleteDataHandle', h)
+        end
         function [ns, ny, nc] = slice_dimensions(self)
             ptr_i = libpointer('int32Ptr', ones(16, 1));
             calllib...
