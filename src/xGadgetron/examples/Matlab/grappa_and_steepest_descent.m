84776ca9f8551b9dbb09fe74dbe1f271844fe124
David Atkinson <D.Atkinson@ucl.ac.uk>
2017-02-19 12:47:54 +0000

    updated grappa_and_steepest_descent demo, checked for ImProcToolbox in acquisitions_handling

--- src/xGadgetron/examples/Matlab/grappa_and_steepest_descent.m
+++ src/xGadgetron/examples/Matlab/grappa_and_steepest_descent.m
@@ -1,76 +1,74 @@
+function grappa_and_steepest_descent
 % GRAPPA reconstruction with the steepest descent step
 % to illustrate the use of Acquisition Model projections.
 
-select_gadgetron
+ccp_libload
 
-try
+% Import SIRF MATLAB Gadgetron package
+import mGadgetron.*
 
-    % define raw data source
-    [filename, pathname] = uigetfile('*.h5', 'Select raw data file');
-    input_data = AcquisitionData(fullfile(pathname, filename));
+% define raw data source
+[filename, pathname] = uigetfile('*.h5', 'Select raw data file');
+input_data = AcquisitionData(fullfile(pathname, filename));
 
-    % pre-process acquisitions
-    fprintf('---\n preprocessing...\n');
-    preprocessed_data = preprocess_acquisitions(input_data);
-    pp_norm = preprocessed_data.norm();
+% pre-process acquisitions
+fprintf('---\n preprocessing...\n');
+preprocessed_data = preprocess_acquisitions(input_data);
+pp_norm = preprocessed_data.norm();
 
-    % perform reconstruction
-    recon = GenericCartesianGRAPPAReconstruction();
-    recon.compute_gfactors(false);
-    recon.set_input(preprocessed_data);
-    fprintf('---\n reconstructing...\n');
-    recon.process();
-    complex_images = recon.get_output();
+% perform reconstruction
+recon = GenericCartesianGRAPPAReconstruction();
+recon.compute_gfactors(false);
+recon.set_input(preprocessed_data);
+fprintf('---\n reconstructing...\n');
+recon.process();
+complex_images = recon.get_output();
 
-    % compute coil sensitivity maps
-    csms = CoilSensitivityMaps();
-    fprintf('---\n sorting acquisitions...\n')
-    preprocessed_data.sort()
-    fprintf('---\n calculating sensitivity maps...\n')
-    csms.calculate(preprocessed_data)
+% compute coil sensitivity maps
+csms = CoilSensitivityMaps();
+fprintf('---\n sorting acquisitions...\n')
+preprocessed_data.sort()
+fprintf('---\n calculating sensitivity maps...\n')
+csms.calculate(preprocessed_data)
 
-    % create acquisition model based on the acquisition parameters
-    % stored in preprocessed_data and image parameters stored in complex_images
-    am = AcquisitionModel(preprocessed_data, complex_images);
-    am.set_coil_sensitivity_maps(csms)
+% create acquisition model based on the acquisition parameters
+% stored in preprocessed_data and image parameters stored in complex_images
+am = AcquisitionModel(preprocessed_data, complex_images);
+am.set_coil_sensitivity_maps(csms)
 
-    % use the acquisition model (forward projection) to simulate acquisitions
-    fwd_data = am.forward(complex_images);
-    fwd_norm = fwd_data.norm();
-    % compute the difference between real and simulated acquisitions
-    diff = fwd_data - preprocessed_data * (fwd_norm/pp_norm);
-    rr = diff.norm()/fwd_norm;
-    fprintf('---\n reconstruction residual norm (rel): %e\n', rr)
+% use the acquisition model (forward projection) to simulate acquisitions
+fwd_data = am.forward(complex_images);
+fwd_norm = fwd_data.norm();
+% compute the difference between real and simulated acquisitions
+diff = fwd_data - preprocessed_data * (fwd_norm/pp_norm);
+rr = diff.norm()/fwd_norm;
+fprintf('---\n reconstruction residual norm (rel): %e\n', rr)
 
-    % try to improve the reconstruction by the steepest descent step
-    g = am.backward(diff);
-    w = am.forward(g);
-    alpha = (g*g)/(w*w);
-    r_complex_imgs = complex_images - g*alpha;
+% try to improve the reconstruction by the steepest descent step
+g = am.backward(diff);
+w = am.forward(g);
+alpha = (g*g)/(w*w);
+r_complex_imgs = complex_images - g*alpha;
 
-    idata = abs(complex_images.as_array());
-    rdata = abs(r_complex_imgs.as_array());
-    idata = idata/max(max(max(idata)));
-    rdata = rdata/max(max(max(rdata)));
-    n = complex_images.number();
+idata = abs(complex_images.as_array());
+rdata = abs(r_complex_imgs.as_array());
+idata = idata/max(max(max(idata)));
+rdata = rdata/max(max(max(rdata)));
+n = complex_images.number();
 
-    fprintf('---\nEnter slice number to view it\n')
-    fprintf('(a value outside the range [1 : %d] will stop this loop)\n', n)
-    while (true)
-        i = input('slice: ');
-        if i < 1 || i > n
-            break
-        end
-        figure(i)
-        imshow(idata(:,:,i));
-        title('GRAPPA image')
-        figure(i + n)
-        imshow(rdata(:,:,i));
-        title('refined image')
+fprintf('---\nEnter slice number to view it\n')
+fprintf('(a value outside the range [1 : %d] will stop this loop)\n', n)
+while (true)
+    i = input('slice: ');
+    if i < 1 || i > n
+        break
     end
-    
-catch err
-    % display error information
-    fprintf('%s\n', err.message)
-    fprintf('error id is %s\n', err.identifier)
+    figure(i)
+    imshow(idata(:,:,i));
+    title('GRAPPA image')
+    figure(i + n)
+    imshow(rdata(:,:,i));
+    title('refined image')
 end
+
+
