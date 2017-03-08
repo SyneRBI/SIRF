659cef5f0fa4871807549071570b1cc22ecff850
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2017-03-08 10:44:09 +0000

    corrected simplistic petmr demo

--- examples/Python/PETMR/simplistic_petmr.py
+++ examples/Python/PETMR/simplistic_petmr.py
@@ -9,27 +9,32 @@ Options:
   --mr_path=<path>    path to MR data files, defaults to data/examples/MR
                       subfolder of SIRF root folder
   --mr_engine=<mr>    MR reconstruction engine [default: Gadgetron]
-  --pet_engine=<pet>  reconstruction engine [default: Stir]
+  --pet_engine=<pet>  PET reconstruction engine [default: Stir]
 '''
 
+# import common (engine-independent) utilities
 import pUtil
 
+# get command-line arguments
 __version__ = '0.1.0'
 from docopt import docopt
 args = docopt(__doc__, version=__version__)
+
+# find MR raw data file
 data_file = args['--file']
 data_path = args['--mr_path']
 if data_path is None:
     data_path = pUtil.petmr_data_path('mr')
 input_file = pUtil.existing_filepath(data_path, data_file)
 
+# import MR and PET engines
 exec('import p' + args['--mr_engine' ] + ' as MR' )
 exec('import p' + args['--pet_engine'] + ' as PET')
 
 def main():
 
 # MR
-    # specify the input data file
+    # specify the MR raw data source
     input_data = MR.AcquisitionData(input_file)
     # pre-process acquisitions
     processed_data = MR.preprocess_acquisitions(input_data)
@@ -55,5 +60,3 @@ try:
     main()
 except MR.error as err:
     print('exception occured: %s' % err.value)
-    
-    
