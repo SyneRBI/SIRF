d891ecd3e5e0631a8e2d6ba775c0ee9455935682
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2017-03-24 10:16:48 +0000

    eliminated vulnerability in setup_MR/PET

--- src/common/setup_MR.m
+++ src/common/setup_MR.m
@@ -10,11 +10,13 @@ function import_str = setup_MR(engine, alias)
     if nargin < 2
         import_str = ['import m' engine '.*'];
     else
-        filename = mfilename();
-        filepath = mfilename('fullpath');
-        l = length(filepath) - length(filename);
-        path = filepath(1:l);
-        copyfile([path '/+m' engine], [path '/+' alias], 'f')
+        if ~strcmp(['m' engine], alias)
+            filename = mfilename();
+            filepath = mfilename('fullpath');
+            l = length(filepath) - length(filename);
+            path = filepath(1:l);
+            copyfile([path '/+m' engine], [path '/+' alias], 'f')
+        end
         import_str = ' ';
     end
 end
