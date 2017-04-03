07fd8d87d660a5198058600b817c33d432b32141
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2017-04-03 15:33:58 +0100

    inserted license statement at the beginning of Matlab m-files

--- src/xSTIR/mSTIR/+mStir/PoissonLogLikelihoodWithLinearModelForMean.m
+++ src/xSTIR/mSTIR/+mStir/PoissonLogLikelihoodWithLinearModelForMean.m
@@ -1,6 +1,25 @@
 classdef PoissonLogLikelihoodWithLinearModelForMean < mStir.ObjectiveFunction
-%     Class for STIR PoissonLogLikelihoodWithLinearModelForMean object, see
-%     http://stir.sourceforge.net/documentation/doxy/html/classstir_1_1PoissonLogLikelihoodWithLinearModelForMean.html
+% Class for STIR PoissonLogLikelihoodWithLinearModelForMean object, see
+% http://stir.sourceforge.net/documentation/doxy/html/classstir_1_1PoissonLogLikelihoodWithLinearModelForMean.html
+
+% CCP PETMR Synergistic Image Reconstruction Framework (SIRF).
+% Copyright 2015 - 2017 Rutherford Appleton Laboratory STFC.
+% Copyright 2015 - 2017 University College London.
+% 
+% This is software developed for the Collaborative Computational
+% Project in Positron Emission Tomography and Magnetic Resonance imaging
+% (http://www.ccppetmr.ac.uk/).
+% 
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+% http://www.apache.org/licenses/LICENSE-2.0
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
     methods
         function self = PoissonLogLikelihoodWithLinearModelForMean()
             self.handle = [];
--- src/xSTIR/mSTIR/+mStir/TruncateToCylinderProcessor.m
+++ src/xSTIR/mSTIR/+mStir/TruncateToCylinderProcessor.m
@@ -1,6 +1,25 @@
 classdef TruncateToCylinderProcessor < mStir.ImageDataProcessor
-%     Class for the image filter that zeroes the image outside the cylinder
-%     of the same xy-diameter and z-size as those of the image.
+% Class for the image filter that zeroes the image outside the cylinder
+% of the same xy-diameter and z-size as those of the image.
+
+% CCP PETMR Synergistic Image Reconstruction Framework (SIRF).
+% Copyright 2015 - 2017 Rutherford Appleton Laboratory STFC.
+% Copyright 2015 - 2017 University College London.
+% 
+% This is software developed for the Collaborative Computational
+% Project in Positron Emission Tomography and Magnetic Resonance imaging
+% (http://www.ccppetmr.ac.uk/).
+% 
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+% http://www.apache.org/licenses/LICENSE-2.0
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
     methods
         function self = TruncateToCylinderProcessor()
             self.name = 'TruncateToCylindricalFOVImageProcessor';
