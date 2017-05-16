cb33ee38e22fe2d042b531e320cd762a33f1f684
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2017-05-16 16:22:12 +0100

    tidied up mUtil.show_3D_array()

--- src/common/+mUtil/show_3D_array.m
+++ src/common/+mUtil/show_3D_array.m
@@ -21,75 +21,57 @@ function show_3D_array(array, sup_title, x_label, y_label, label)
 shape = size(array);
 nx = shape(1);
 ny = shape(2);
-% figure;
-% imshow(ones(nx, ny))
-% axis on
-% xlabel('readouts')
-% ylabel('samples')
-% title([sup_title ': axes data'])
 
 if ndims(array) < 3
     nz = uint16(1);
 else
     nz = uint16(shape(3));
 end
-if nz > 1
-    n = nz + 1;
-else
-    n = nz;
+s = double(nz*ny/nx);
+irows = uint16(round(sqrt(s)));
+if irows < 1
+    irows = uint16(1);
 end
-s = double(n*ny/nx);
-rows = uint16(round(sqrt(s)));
-% extra_row = 0;
-% if nz == 1
-%     rows = uint16(2);
-%     extra_row = 1;
-% end
-if rows < 1
-    rows = uint16(1);
+if irows > nz
+    irows = uint16(nz);
 end
-if rows > n
-    rows = uint16(n);
-end
-cols = idivide(n - 1, rows) + 1;
-rows = double(rows);
-cols = double(cols);
-h = figure;
+icols = idivide(nz - 1, irows) + 1;
+rows = double(irows);
+cols = double(icols);
+
+f = figure;
+panel = uipanel('Parent', f, 'BorderType', 'none');
+panel.Title = sup_title;
+panel.TitlePosition = 'centertop'; 
+panel.FontSize = 12;
+panel.FontWeight = 'bold';
+
 scale = max(abs(array(:)))/255;
-for z = 1 : n
-    p = double(z); % + extra_row*cols;
-    subplot(rows, cols, p)
+for z = 1 : nz
+    row = idivide(z - 1, icols) + 1;
+    col = z - (row - 1)*icols;
+    p = double(z);
+    subplot(rows, cols, p, 'Parent', panel)
     if z <= nz
         image = uint8(array(:,:,z)/scale);
         imshow(image, 'Colormap', jet(255))
     else
         imshow(ones(nx, ny), 'Colormap', jet(255))
     end
-    if z == n || nz == 1
+    if row == irows && col == 1
         xlabel(y_label)
         ylabel(x_label)
         set(gca, 'XTick', [1 ny])
         set(gca, 'YTick', [1 nx])
         axis on
     end
-    if nz > 1 && z < n
+    if nz > 1
         if nargin > 4
             sub_title = sprintf('%s %d', label, z);
         else
             sub_title = sprintf('%d', z);
         end
-        %t = title(sub_title);
-        t = title('');
-        set(t, 'FontSize', 8);
-        tpos = round(get(t, 'Position'));
-        text(tpos(1), tpos(2) + nx + 50, sub_title, 'HorizontalAlignment',...
-            'center');
+        title(sub_title);
     end
 end
-h.NextPlot = 'add';
-a = axes;
-ht = title(sup_title);
-a.Visible = 'off';
-ht.Visible = 'on';
-%set(ht, 'FontSize', 12);
 end
