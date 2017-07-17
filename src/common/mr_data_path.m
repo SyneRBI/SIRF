dec9cf45675d093f400e2740edd0165ada2c1f45
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2017-07-17 11:14:28 +0100

    responded to #44

--- src/common/mr_data_path.m
+++ src/common/mr_data_path.m
@@ -1,5 +1,9 @@
 function p = mr_data_path
-% Tries to find path to MR raw data
+% Tries to find path to MR raw data.
+% The user may like to set a Matlab variable SIRF_MR_DATA_PATH
+% to the path to their raw MR data.
+% If it is not set, the path to SIRF subfolder /data/examples/MR
+% will be used.
 
 if exist('SIRF_MR_DATA_PATH', 'var')
     p = SIRF_MR_DATA_PATH;
