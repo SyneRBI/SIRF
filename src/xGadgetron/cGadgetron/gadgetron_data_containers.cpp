12ee15512b3529c5e3d0688226779e18126d9d78
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2017-10-04 12:45:53 +0100

    re-arranged some xGadgetron sources

--- src/xGadgetron/cGadgetron/gadgetron_data_containers.cpp
+++ src/xGadgetron/cGadgetron/gadgetron_data_containers.cpp
@@ -34,54 +34,54 @@ using namespace gadgetron;
 shared_ptr<AcquisitionsContainer> 
 AcquisitionsContainer::acqs_templ_;
 
-void 
-ImageWrap::get_cmplx_data(float* re, float* im) const
-{
-	int dim[4];
-	size_t n = get_dim(dim);
-	if (type_ == ISMRMRD::ISMRMRD_CXFLOAT) {
-		const CFImage& img = *(const CFImage*)ptr_;
-		const complex_float_t* ptr = img.getDataPtr();
-		for (size_t i = 0; i < n; i++) {
-			complex_float_t z = ptr[i];
-			re[i] = std::real(z);
-			im[i] = std::imag(z);
-		}
-	}
-	else if (type_ == ISMRMRD::ISMRMRD_CXDOUBLE) {
-		const CDImage& img = *(const CDImage*)ptr_;
-		const complex_double_t* ptr = img.getDataPtr();
-		for (size_t i = 0; i < n; i++) {
-			complex_double_t z = ptr[i];
-			re[i] = std::real(z);
-			im[i] = std::imag(z);
-		}
-	}
-	else {
-		get_data(re);
-		for (size_t i = 0; i < n; i++)
-			im[i] = 0;
-	}
-}
-
-void 
-ImageWrap::set_cmplx_data(const float* re, const float* im) const
-{
-	int dim[4];
-	size_t n = get_dim(dim);
-	if (type_ == ISMRMRD::ISMRMRD_CXFLOAT) {
-		CFImage& img = *(CFImage*)ptr_;
-		complex_float_t* ptr = img.getDataPtr();
-		for (size_t i = 0; i < n; i++)
-			ptr[i] = std::complex<float>((float)re[i], (float)im[i]);
-	}
-	else if (type_ == ISMRMRD::ISMRMRD_CXDOUBLE) {
-		CDImage& img = *(CDImage*)ptr_;
-		complex_double_t* ptr = img.getDataPtr();
-		for (size_t i = 0; i < n; i++)
-			ptr[i] = std::complex<double>(re[i], im[i]);
-	}
-}
+//void 
+//ImageWrap::get_cmplx_data(float* re, float* im) const
+//{
+//	int dim[4];
+//	size_t n = get_dim(dim);
+//	if (type_ == ISMRMRD::ISMRMRD_CXFLOAT) {
+//		const CFImage& img = *(const CFImage*)ptr_;
+//		const complex_float_t* ptr = img.getDataPtr();
+//		for (size_t i = 0; i < n; i++) {
+//			complex_float_t z = ptr[i];
+//			re[i] = std::real(z);
+//			im[i] = std::imag(z);
+//		}
+//	}
+//	else if (type_ == ISMRMRD::ISMRMRD_CXDOUBLE) {
+//		const CDImage& img = *(const CDImage*)ptr_;
+//		const complex_double_t* ptr = img.getDataPtr();
+//		for (size_t i = 0; i < n; i++) {
+//			complex_double_t z = ptr[i];
+//			re[i] = std::real(z);
+//			im[i] = std::imag(z);
+//		}
+//	}
+//	else {
+//		get_data(re);
+//		for (size_t i = 0; i < n; i++)
+//			im[i] = 0;
+//	}
+//}
+//
+//void 
+//ImageWrap::set_cmplx_data(const float* re, const float* im) const
+//{
+//	int dim[4];
+//	size_t n = get_dim(dim);
+//	if (type_ == ISMRMRD::ISMRMRD_CXFLOAT) {
+//		CFImage& img = *(CFImage*)ptr_;
+//		complex_float_t* ptr = img.getDataPtr();
+//		for (size_t i = 0; i < n; i++)
+//			ptr[i] = std::complex<float>((float)re[i], (float)im[i]);
+//	}
+//	else if (type_ == ISMRMRD::ISMRMRD_CXDOUBLE) {
+//		CDImage& img = *(CDImage*)ptr_;
+//		complex_double_t* ptr = img.getDataPtr();
+//		for (size_t i = 0; i < n; i++)
+//			ptr[i] = std::complex<double>(re[i], im[i]);
+//	}
+//}
 
 bool 
 AcquisitionsContainer::undersampled() const
