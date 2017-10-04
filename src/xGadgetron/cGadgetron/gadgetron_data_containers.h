48ee2d3a7efd7d097581099bb1050a55dcafe65a
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2017-10-04 13:49:33 +0100

    some more source re-arrangement in cGadgetron

--- src/xGadgetron/cGadgetron/gadgetron_data_containers.h
+++ src/xGadgetron/cGadgetron/gadgetron_data_containers.h
@@ -30,48 +30,18 @@ limitations under the License.
 #ifndef GADGETRON_DATA_CONTAINERS
 #define GADGETRON_DATA_CONTAINERS
 
-#include <algorithm> // stable_sort
-#include <array> // array
-#include <numeric> // iota
-#include <vector> // vector
-
-namespace Multisort {
-
-	template<typename T, size_t N>
-	bool less(const std::array<T, N>& a, const std::array<T, N>& b)
-	{
-		for (unsigned int i = 0; i < N; i++) {
-			if (a[i] < b[i])
-				return true;
-			if (a[i] > b[i])
-				return false;
-		}
-		return false; // all equal
-	}
-
-	template<typename T, size_t N>
-	void sort(std::vector<std::array<T, N> > v, int* index)
-	{
-		int n = v.size();
-		std::iota(index, index + n, 0);
-		std::stable_sort
-			(index, index + n, [&v](int i, int j){return less(v[i], v[j]); });
-	}
-
-} // namespace Multisort
-
-#include <complex>
-#include <fstream>
-#include <map>
-#include <thread>
-#include <chrono>
-#include <condition_variable>
-
-#include <boost/thread/thread.hpp>
-#include <boost/thread/mutex.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/algorithm/string/predicate.hpp>
-#include <boost/algorithm/string/replace.hpp>
+//#include <complex>
+//#include <fstream>
+//#include <map>
+//#include <thread>
+//#include <chrono>
+//#include <condition_variable>
+//
+//#include <boost/thread/thread.hpp>
+//#include <boost/thread/mutex.hpp>
+//#include <boost/shared_ptr.hpp>
+//#include <boost/algorithm/string/predicate.hpp>
+//#include <boost/algorithm/string/replace.hpp>
 
 #include <ismrmrd/ismrmrd.h>
 #include <ismrmrd/dataset.h>
@@ -82,8 +52,8 @@ namespace Multisort {
 #include "cgadgetron_shared_ptr.h"
 #include "gadgetron_image_wrap.h"
 #include "SIRF/common/data_container.h"
+#include "SIRF/common/multisort.h"
 
-//using namespace SPTR_NAMESPACE;
 using namespace gadgetron;
 
 #define TO_BE_IGNORED(acq) \
@@ -91,399 +61,6 @@ using namespace gadgetron;
 	!(acq).isFlagSet(ISMRMRD::ISMRMRD_ACQ_IS_PARALLEL_CALIBRATION_AND_IMAGING) && \
 	!(acq).isFlagSet(ISMRMRD::ISMRMRD_ACQ_LAST_IN_MEASUREMENT) && \
 	(acq).flags() >= (1 << (ISMRMRD::ISMRMRD_ACQ_IS_NOISE_MEASUREMENT - 1)))
-
-//#define IMAGE_PROCESSING_SWITCH(Type, Operation, Arguments, ...)\
-//	if (Type == ISMRMRD::ISMRMRD_USHORT)\
-//		Operation ((ISMRMRD::Image<unsigned short>*) Arguments, ##__VA_ARGS__);\
-//	else if (Type == ISMRMRD::ISMRMRD_SHORT)\
-//		Operation ((ISMRMRD::Image<short>*) Arguments, ##__VA_ARGS__);\
-//	else if (Type == ISMRMRD::ISMRMRD_UINT)\
-//		Operation ((ISMRMRD::Image<unsigned int>*) Arguments, ##__VA_ARGS__);\
-//	else if (Type == ISMRMRD::ISMRMRD_INT)\
-//		Operation ((ISMRMRD::Image<int>*) Arguments, ##__VA_ARGS__);\
-//	else if (Type == ISMRMRD::ISMRMRD_FLOAT)\
-//		Operation ((ISMRMRD::Image<float>*) Arguments, ##__VA_ARGS__);\
-//	else if (Type == ISMRMRD::ISMRMRD_DOUBLE)\
-//		Operation ((ISMRMRD::Image<double>*) Arguments, ##__VA_ARGS__);\
-//	else if (Type == ISMRMRD::ISMRMRD_CXFLOAT)\
-//		Operation ((ISMRMRD::Image< std::complex<float> >*) Arguments, ##__VA_ARGS__);\
-//	else if (Type == ISMRMRD::ISMRMRD_CXDOUBLE)\
-//		Operation ((ISMRMRD::Image< std::complex<double> >*) Arguments, ##__VA_ARGS__);
-//
-//#define IMAGE_PROCESSING_SWITCH_CONST(Type, Operation, Arguments, ...)\
-//	if (Type == ISMRMRD::ISMRMRD_USHORT)\
-//		Operation ((const ISMRMRD::Image<unsigned short>*) Arguments, ##__VA_ARGS__);\
-//		else if (Type == ISMRMRD::ISMRMRD_SHORT)\
-//		Operation ((const ISMRMRD::Image<short>*) Arguments, ##__VA_ARGS__);\
-//		else if (Type == ISMRMRD::ISMRMRD_UINT)\
-//		Operation ((const ISMRMRD::Image<unsigned int>*) Arguments, ##__VA_ARGS__);\
-//		else if (Type == ISMRMRD::ISMRMRD_INT)\
-//		Operation ((const ISMRMRD::Image<int>*) Arguments, ##__VA_ARGS__);\
-//		else if (Type == ISMRMRD::ISMRMRD_FLOAT)\
-//		Operation ((const ISMRMRD::Image<float>*) Arguments, ##__VA_ARGS__);\
-//		else if (Type == ISMRMRD::ISMRMRD_DOUBLE)\
-//		Operation ((const ISMRMRD::Image<double>*) Arguments, ##__VA_ARGS__);\
-//		else if (Type == ISMRMRD::ISMRMRD_CXFLOAT)\
-//		Operation ((const ISMRMRD::Image< std::complex<float> >*) Arguments, ##__VA_ARGS__);\
-//		else if (Type == ISMRMRD::ISMRMRD_CXDOUBLE)\
-//		Operation ((const ISMRMRD::Image< std::complex<double> >*) Arguments, ##__VA_ARGS__);
-//
-//typedef ISMRMRD::Image<complex_float_t> CFImage;
-//typedef ISMRMRD::Image<complex_double_t> CDImage;
-
-//class xGadgetronUtilities {
-//public:
-//	static long long milliseconds()
-//	{
-//		auto now = std::chrono::system_clock::now();
-//		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
-//		return (long long)ms.count();
-//	}
-//	static std::string scratch_file_name()
-//	{
-//		static int calls = 0;
-//		char buff[32];
-//		long long int ms = xGadgetronUtilities::milliseconds();
-//		calls++;
-//		sprintf(buff, "tmp_%d_%lld.h5", calls, ms);
-//		return std::string(buff);
-//	}
-//	template<typename T>
-//	static void convert_complex(std::complex<T> z, unsigned short& t)
-//	{
-//		t = (unsigned short)z.real();
-//	}
-//	template<typename T>
-//	static void convert_complex(std::complex<T> z, short& t)
-//	{
-//		t = (short)z.real();
-//	}
-//	template<typename T>
-//	static void convert_complex(std::complex<T> z, unsigned int& t)
-//	{
-//		t = (unsigned int)z.real();
-//	}
-//	template<typename T>
-//	static void convert_complex(std::complex<T> z, int& t)
-//	{
-//		t = (int)z.real();
-//	}
-//	template<typename T>
-//	static void convert_complex(std::complex<T> z, float& t)
-//	{
-//		t = (float)z.real();
-//	}
-//	template<typename T>
-//	static void convert_complex(std::complex<T> z, complex_float_t& t)
-//	{
-//		t = (complex_float_t)z;
-//	}
-//	template<typename T>
-//	static void convert_complex(std::complex<T> z, double& t)
-//	{
-//		t = (double)z.real();
-//	}
-//	template<typename T>
-//	static void convert_complex(std::complex<T> z, complex_double_t& t)
-//	{
-//		t = (complex_double_t)z;
-//	}
-//
-//};
-//
-//class Mutex {
-//public:
-//	Mutex() 
-//	{
-//		init_();
-//	}
-//	boost::mutex& operator()() 
-//	{
-//		return *sptr_mutex_.get();
-//	}
-//	shared_ptr<boost::mutex> sptr() 
-//	{
-//		return sptr_mutex_;
-//	}
-//	void lock()
-//	{
-//		sptr_mutex_->lock();
-//	}
-//	void unlock()
-//	{
-//		sptr_mutex_->unlock();
-//	}
-//private:
-//	static shared_ptr<boost::mutex> sptr_mutex_;
-//	static void init_() 
-//	{
-//		static bool initialized = false;
-//		if (!initialized) {
-//			sptr_mutex_ = shared_ptr<boost::mutex>(new boost::mutex);
-//			initialized = true;
-//		}
-//	}
-//};
-//
-//class ImageWrap {
-//public:
-//	ImageWrap(uint16_t type = 0, void* ptr_im = 0)
-//	{
-//		type_ = type;
-//		ptr_ = ptr_im;
-//	}
-//	ImageWrap(const ImageWrap& iw) 
-//	{
-//		type_ = iw.type();
-//		IMAGE_PROCESSING_SWITCH(type_, copy_, iw.ptr_image());
-//	}
-//	~ImageWrap()
-//	{
-//		IMAGE_PROCESSING_SWITCH(type_, delete, ptr_);
-//	}
-//	int type() const
-//	{
-//		return type_;
-//	}
-//	void* ptr_image()
-//	{
-//		return ptr_;
-//	}
-//	const void* ptr_image() const
-//	{
-//		return ptr_;
-//	}
-//	size_t size() const
-//	{
-//		size_t s;
-//		IMAGE_PROCESSING_SWITCH_CONST(type_, get_size_, ptr_, s);
-//		return s;
-//	}
-//	ISMRMRD::ImageHeader* ptr_head()
-//	{
-//		ISMRMRD::ImageHeader** h;
-//		IMAGE_PROCESSING_SWITCH(type_, get_head_ptr_, ptr_, h);
-//		return *h;
-//	}
-//	std::string attributes() const
-//	{
-//		std::string attr;
-//		IMAGE_PROCESSING_SWITCH_CONST(type_, get_attr_, ptr_, attr);
-//		return attr;
-//	}
-//	void set_imtype(ISMRMRD::ISMRMRD_ImageTypes imtype)
-//	{
-//		IMAGE_PROCESSING_SWITCH(type_, set_imtype_, ptr_, imtype);
-//	}
-//	size_t get_dim(int* dim) const
-//	{
-//		IMAGE_PROCESSING_SWITCH_CONST(type_, get_dim_, ptr_, dim);
-//		size_t n = dim[0];
-//		n *= dim[1];
-//		n *= dim[2];
-//		n *= dim[3];
-//		return n;
-//	}
-//	void get_data(float* data) const
-//	{
-//		IMAGE_PROCESSING_SWITCH_CONST(type_, get_data_, ptr_, data);
-//	}
-//	void write(ISMRMRD::Dataset& dataset) const
-//	{
-//		IMAGE_PROCESSING_SWITCH_CONST(type_, write_, ptr_, dataset);
-//	}
-//	void axpby(complex_float_t a, const ImageWrap& x, complex_float_t b)
-//	{
-//		IMAGE_PROCESSING_SWITCH(type_, axpby_, x.ptr_image(), a, b);
-//	}
-//	complex_float_t dot(const ImageWrap& iw) const
-//	{
-//		complex_float_t z;
-//		IMAGE_PROCESSING_SWITCH_CONST(type_, dot_, iw.ptr_image(), &z);
-//		return z;
-//	}
-//	float norm() const
-//	{
-//		float r;
-//		IMAGE_PROCESSING_SWITCH_CONST(type_, norm_, ptr_, &r);
-//		return r;
-//	}
-//	float diff(ImageWrap& iw) const
-//	{
-//		float s;
-//		IMAGE_PROCESSING_SWITCH_CONST(type_, diff_, iw.ptr_image(), &s);
-//		return s;
-//	}
-//
-//	void get_cmplx_data(float* re, float* im) const;
-//
-//	void set_cmplx_data(const float* re, const float* im) const;
-//
-//private:
-//	int type_;
-//	void* ptr_;
-//
-//	ImageWrap& operator=(const ImageWrap& iw)
-//	{
-//		type_ = iw.type();
-//		IMAGE_PROCESSING_SWITCH(type_, copy_, iw.ptr_image());
-//		return *this;
-//	}
-//
-//	template<typename T>
-//	void copy_(const ISMRMRD::Image<T>* ptr_im)
-//	{
-//		type_ = ptr_im->getDataType();
-//		ptr_ = (void*)new ISMRMRD::Image<T>(*ptr_im);
-//	}
-//
-//	template<typename T>
-//	void get_head_ptr_(ISMRMRD::Image<T>* ptr_im, ISMRMRD::ImageHeader** h)
-//	{
-//		*h = &(ptr_im->getHead());
-//	}
-//
-//	template<typename T>
-//	void set_imtype_(ISMRMRD::Image<T>* ptr_im, ISMRMRD::ISMRMRD_ImageTypes type)
-//	{
-//		ptr_im->setImageType(type);
-//	}
-//
-//	template<typename T>
-//	void get_size_(const ISMRMRD::Image<T>* ptr_im, size_t& size) const
-//	{
-//		size = ptr_im->getDataSize();
-//	}
-//
-//	template<typename T>
-//	void get_attr_(const ISMRMRD::Image<T>* ptr_im, std::string& attr) const
-//	{
-//		ptr_im->getAttributeString(attr);
-//	}
-//
-//	template<typename T>
-//	void write_
-//		(const ISMRMRD::Image<T>* ptr_im, ISMRMRD::Dataset& dataset) const
-//	{
-//		//std::cout << "appending image..." << std::endl;
-//		const ISMRMRD::Image<T>& im = *ptr_im;
-//		std::stringstream ss;
-//		ss << "image_" << im.getHead().image_series_index;
-//		std::string image_varname = ss.str();
-//		{
-//			Mutex mtx;
-//			mtx.lock();
-//			dataset.appendImage(image_varname, im);
-//			mtx.unlock();
-//		}
-//	}
-//
-//	template<typename T>
-//	void get_dim_(const ISMRMRD::Image<T>* ptr_im, int* dim) const
-//	{
-//		const ISMRMRD::Image<T>& im = *(const ISMRMRD::Image<T>*)ptr_im;
-//		dim[0] = im.getMatrixSizeX();
-//		dim[1] = im.getMatrixSizeY();
-//		dim[2] = im.getMatrixSizeZ();
-//		dim[3] = im.getNumberOfChannels();
-//	}
-//
-//	template<typename T>
-//	void get_data_(const ISMRMRD::Image<T>* ptr_im, float* data) const
-//	{
-//		const ISMRMRD::Image<T>& im = *ptr_im;
-//		const T* ptr = im.getDataPtr();
-//		size_t n = im.getNumberOfDataElements();
-//		for (size_t i = 0; i < n; i++)
-//			data[i] = std::real(ptr[i]);
-//	}
-//
-//	template<typename T>
-//	void axpby_
-//		(const ISMRMRD::Image<T>* ptr_x, complex_float_t a, complex_float_t b)
-//	{
-//		ISMRMRD::Image<T>* ptr_y = (ISMRMRD::Image<T>*)ptr_;
-//		const T* i;
-//		T* j;
-//		size_t ii = 0;
-//		size_t n = ptr_x->getNumberOfDataElements();
-//		if (b == complex_float_t(0.0))
-//			for (i = ptr_x->getDataPtr(), j = ptr_y->getDataPtr(); ii < n;
-//				i++, j++, ii++) {
-//			complex_float_t u = (complex_float_t)*i;
-//			xGadgetronUtilities::convert_complex(a*u, *j);
-//		}
-//		else
-//			for (i = ptr_x->getDataPtr(), j = ptr_y->getDataPtr(); ii < n;
-//				i++, j++, ii++) {
-//			complex_float_t u = (complex_float_t)*i;
-//			complex_float_t v = (complex_float_t)*j;
-//			xGadgetronUtilities::convert_complex(a*u + b*v, *j);
-//		}
-//	}
-//
-//	template<typename T>
-//	void dot_(const ISMRMRD::Image<T>* ptr_im, complex_float_t *z) const
-//	{
-//		const ISMRMRD::Image<T>* ptr = (const ISMRMRD::Image<T>*)ptr_;
-//		const T* i;
-//		const T* j;
-//		*z = 0;
-//		size_t ii = 0;
-//		size_t n = ptr_im->getNumberOfDataElements();
-//		for (i = ptr->getDataPtr(), j = ptr_im->getDataPtr(); ii < n;
-//			i++, j++, ii++) {
-//			complex_float_t u = (complex_float_t)*i;
-//			complex_float_t v = (complex_float_t)*j;
-//			*z += std::conj(v) * u;
-//		}
-//	}
-//
-//	template<typename T>
-//	void norm_(const ISMRMRD::Image<T>* ptr, float *r) const
-//	{
-//		const T* i;
-//		*r = 0;
-//		size_t ii = 0;
-//		size_t n = ptr->getNumberOfDataElements();
-//		for (i = ptr->getDataPtr(); ii < n; i++, ii++) {
-//			complex_float_t a = (complex_float_t)*i;
-//			*r += std::abs(std::conj(a) * a);
-//		}
-//		*r = std::sqrt(*r);
-//	}
-//
-//	template<typename T>
-//	void diff_(const ISMRMRD::Image<T>* ptr_im, float *s) const
-//	{
-//		const ISMRMRD::Image<T>* ptr = (const ISMRMRD::Image<T>*)ptr_;
-//		const T* i;
-//		const T* j;
-//		*s = 0;
-//		size_t ii = 0;
-//		size_t n = ptr_im->getNumberOfDataElements();
-//		for (i = ptr->getDataPtr(), j = ptr_im->getDataPtr(); ii < n;
-//			i++, j++, ii++) {
-//			complex_float_t a = (complex_float_t)*i;
-//			complex_float_t b = (complex_float_t)*j;
-//			*s += (float)std::abs(b - a);
-//		}
-//	}
-//};
-//
-//template <typename T>
-//class aDataContainer {
-//public:
-//	virtual ~aDataContainer() {}
-//	virtual aDataContainer<T>* new_data_container() = 0;
-//	//virtual shared_ptr<aDataContainer<T> > new_data_container() = 0;
-//	virtual unsigned int items() = 0;
-//	virtual float norm() = 0;
-//	virtual T dot(aDataContainer<T>& dc) = 0;
-//	virtual void axpby(
-//		T a, const aDataContainer<T>& a_x,
-//		T b, const aDataContainer<T>& a_y) = 0;
-//};
 
 class AcquisitionsInfo {
 public:
@@ -605,13 +182,6 @@ public:
 		init();
 		return acqs_templ_->same_acquisitions_container(acqs_info_);
 	}
-	//virtual shared_ptr<aDataContainer<complex_float_t> > 
-	//	new_data_container()
-	//{
-	//	init();
-	//	return shared_ptr<aDataContainer<complex_float_t> >
-	//		(acqs_templ_->same_acquisitions_container(acqs_info_));
-	//}
 	virtual shared_ptr<AcquisitionsContainer> new_acquisitions_container()
 	{
 		init();
@@ -664,12 +234,6 @@ public:
 		AcquisitionsFile::init();
 		return acqs_templ_->same_acquisitions_container(acqs_info_);
 	}
-	//virtual shared_ptr<aDataContainer<complex_float_t> > new_data_container()
-	//{
-	//	AcquisitionsFile::init();
-	//	return shared_ptr<aDataContainer<complex_float_t> >
-	//		(acqs_templ_->same_acquisitions_container(acqs_info_));
-	//}
 	virtual shared_ptr<AcquisitionsContainer> new_acquisitions_container()
 	{
 		AcquisitionsFile::init();
@@ -792,11 +356,6 @@ public:
 	{
 		return (aDataContainer<complex_float_t>*)new ImagesVector();
 	}
-	//virtual shared_ptr<aDataContainer<complex_float_t> > new_data_container()
-	//{
-	//	return shared_ptr<aDataContainer<complex_float_t> >
-	//		((aDataContainer<complex_float_t>*)new ImagesVector());
-	//}
 	virtual shared_ptr<ImagesContainer> new_images_container()
 	{
 		return shared_ptr<ImagesContainer>((ImagesContainer*)new ImagesVector());
@@ -956,11 +515,6 @@ public:
 	{
 		return (aDataContainer<complex_float_t>*)new CoilImagesVector();
 	}
-	//virtual shared_ptr<aDataContainer<complex_float_t> > new_data_container()
-	//{
-	//	return shared_ptr<aDataContainer<complex_float_t> >
-	//		((aDataContainer<complex_float_t>*)new CoilImagesVector());
-	//}
 	virtual unsigned int items()
 	{
 		return CoilDataVector::items();
@@ -1041,11 +595,6 @@ public:
 	{
 		return (aDataContainer<complex_float_t>*)new CoilSensitivitiesAsImages();
 	}
-	//virtual shared_ptr<aDataContainer<complex_float_t> > new_data_container()
-	//{
-	//	return shared_ptr<aDataContainer<complex_float_t> >
-	//		((aDataContainer<complex_float_t>*)new CoilSensitivitiesAsImages());
-	//}
 
 	virtual unsigned int items()
 	{
--- src/xGadgetron/cGadgetron/gadgetron_image_wrap.h
+++ src/xGadgetron/cGadgetron/gadgetron_image_wrap.h
@@ -1,18 +1,18 @@
 #ifndef GADGETRON_IMAGE_WRAP_TYPE
 #define GADGETRON_IMAGE_WRAP_TYPE
 
-#include <complex>
-#include <fstream>
-#include <map>
-#include <thread>
-#include <chrono>
-#include <condition_variable>
+//#include <complex>
+//#include <fstream>
+//#include <map>
+//#include <thread>
+//#include <chrono>
+//#include <condition_variable>
 
-#include <boost/thread/thread.hpp>
-#include <boost/thread/mutex.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/algorithm/string/predicate.hpp>
-#include <boost/algorithm/string/replace.hpp>
+//#include <boost/thread/thread.hpp>
+//#include <boost/thread/mutex.hpp>
+//#include <boost/shared_ptr.hpp>
+//#include <boost/algorithm/string/predicate.hpp>
+//#include <boost/algorithm/string/replace.hpp>
 
 #include <ismrmrd/ismrmrd.h>
 #include <ismrmrd/dataset.h>
@@ -20,6 +20,7 @@
 #include <ismrmrd/xml.h>
 
 #include "cgadgetron_shared_ptr.h"
+#include "xgadgetron_utilities.h"
 
 #define IMAGE_PROCESSING_SWITCH(Type, Operation, Arguments, ...)\
 	if (Type == ISMRMRD::ISMRMRD_USHORT)\
@@ -61,100 +62,6 @@ typedef ISMRMRD::Image<complex_float_t> CFImage;
 typedef ISMRMRD::Image<complex_double_t> CDImage;
 
 using namespace gadgetron;
-
-class xGadgetronUtilities {
-public:
-	static long long milliseconds()
-	{
-		auto now = std::chrono::system_clock::now();
-		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
-		return (long long)ms.count();
-	}
-	static std::string scratch_file_name()
-	{
-		static int calls = 0;
-		char buff[32];
-		long long int ms = xGadgetronUtilities::milliseconds();
-		calls++;
-		sprintf(buff, "tmp_%d_%lld.h5", calls, ms);
-		return std::string(buff);
-	}
-	template<typename T>
-	static void convert_complex(std::complex<T> z, unsigned short& t)
-	{
-		t = (unsigned short)z.real();
-	}
-	template<typename T>
-	static void convert_complex(std::complex<T> z, short& t)
-	{
-		t = (short)z.real();
-	}
-	template<typename T>
-	static void convert_complex(std::complex<T> z, unsigned int& t)
-	{
-		t = (unsigned int)z.real();
-	}
-	template<typename T>
-	static void convert_complex(std::complex<T> z, int& t)
-	{
-		t = (int)z.real();
-	}
-	template<typename T>
-	static void convert_complex(std::complex<T> z, float& t)
-	{
-		t = (float)z.real();
-	}
-	template<typename T>
-	static void convert_complex(std::complex<T> z, complex_float_t& t)
-	{
-		t = (complex_float_t)z;
-	}
-	template<typename T>
-	static void convert_complex(std::complex<T> z, double& t)
-	{
-		t = (double)z.real();
-	}
-	template<typename T>
-	static void convert_complex(std::complex<T> z, complex_double_t& t)
-	{
-		t = (complex_double_t)z;
-	}
-
-};
-
-class Mutex {
-public:
-	Mutex()
-	{
-		init_();
-	}
-	boost::mutex& operator()()
-	{
-		return *sptr_mutex_.get();
-	}
-	shared_ptr<boost::mutex> sptr()
-	{
-		return sptr_mutex_;
-	}
-	void lock()
-	{
-		sptr_mutex_->lock();
-	}
-	void unlock()
-	{
-		sptr_mutex_->unlock();
-	}
-private:
-	static shared_ptr<boost::mutex> sptr_mutex_;
-	static void init_()
-	{
-		static bool initialized = false;
-		if (!initialized) {
-			sptr_mutex_ = shared_ptr<boost::mutex>(new boost::mutex);
-			initialized = true;
-		}
-	}
-};
 
 class ImageWrap {
 public:
