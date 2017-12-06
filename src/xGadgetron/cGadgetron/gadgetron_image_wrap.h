e958df5fb5052a1399b41a6cff84b757c533e80b
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2017-12-06 12:29:00 +0000

    added missing license preambles

--- src/xGadgetron/cGadgetron/gadgetron_image_wrap.h
+++ src/xGadgetron/cGadgetron/gadgetron_image_wrap.h
@@ -1,18 +1,34 @@
+/*
+CCP PETMR Synergistic Image Reconstruction Framework (SIRF)
+Copyright 2015 - 2017 Rutherford Appleton Laboratory STFC
+
+This is software developed for the Collaborative Computational
+Project in Positron Emission Tomography and Magnetic Resonance imaging
+(http://www.ccppetmr.ac.uk/).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+/*!
+\file
+\ingroup Gadgetron Image Wrapper
+\brief Specification file for a wrapper class for ISMRMRD::Image.
+
+\author Evgueni Ovtchinnikov
+\author CCP PETMR
+*/
+
 #ifndef GADGETRON_IMAGE_WRAP_TYPE
 #define GADGETRON_IMAGE_WRAP_TYPE
-
-//#include <complex>
-//#include <fstream>
-//#include <map>
-//#include <thread>
-//#include <chrono>
-//#include <condition_variable>
-
-//#include <boost/thread/thread.hpp>
-//#include <boost/thread/mutex.hpp>
-//#include <boost/shared_ptr.hpp>
-//#include <boost/algorithm/string/predicate.hpp>
-//#include <boost/algorithm/string/replace.hpp>
 
 #include <ismrmrd/ismrmrd.h>
 #include <ismrmrd/dataset.h>
