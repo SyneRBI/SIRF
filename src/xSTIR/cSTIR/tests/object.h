331cea209b820f93be9bc57808c9c8a84e883e1a
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2017-12-20 12:00:10 +0000

    replaced boost::shared_ptr with stir::shared_ptr in C/C++ tests

--- src/xSTIR/cSTIR/tests/object.h
+++ src/xSTIR/cSTIR/tests/object.h
@@ -2,8 +2,8 @@
 
 //#define CAST_REF(T, X, Y) T& X = (T&)Y
 #define CREATE_OBJ(Obj, X, sptr_X, Par) \
-	boost::shared_ptr< Obj > sptr_X(new Obj(Par)); \
+	stir::shared_ptr< Obj > sptr_X(new Obj(Par)); \
 	Obj& X = (Obj&)*sptr_X
 #define CREATE_OBJECT(Base, Object, X, sptr_X, Par) \
-	boost::shared_ptr< Base > sptr_X(new Object(Par)); \
+	stir::shared_ptr< Base > sptr_X(new Object(Par)); \
 	Object& X = (Object&)*sptr_X
