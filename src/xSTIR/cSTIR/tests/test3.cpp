dd128e720c1e8ca7279f0c3759e9e00f2725648a
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2017-12-22 14:44:31 +0000

    tested C interface to ListmodeToSinograms

--- src/xSTIR/cSTIR/tests/test3.cpp
+++ src/xSTIR/cSTIR/tests/test3.cpp
@@ -23,3 +23,39 @@ int test3()
 
 	return 0;
 }
+
+#include "cstir.h"
+#include "handle.h"
+
+int test3a()
+{
+	void* lm2s = 0;
+	//void* h = 0;
+	void* handle = 0;
+	float interval[] = { 0, 10 };
+
+	for (;;) {
+		HANDLE(lm2s, cSTIR_newObject("ListmodeToSinograms"));
+		handle = charDataHandle("list.l.hdr.STIR");
+		CALL(cSTIR_setParameter
+			(lm2s, "ListmodeToSinograms", "input", handle));
+		deleteDataHandle(handle);
+		handle = charDataHandle("proj_data");
+		CALL(cSTIR_setParameter
+			(lm2s, "ListmodeToSinograms", "output", handle));
+		deleteDataHandle(handle);
+		handle = charDataHandle("template_span11.hs");
+		CALL(cSTIR_setParameter
+			(lm2s, "ListmodeToSinograms", "template", handle));
+		deleteDataHandle(handle);
+		CALL(cSTIR_setListmodeToSinogramsInterval(lm2s, (size_t)interval));
+		CALL(cSTIR_setupListmodeToSinogramsConverter(lm2s));
+		CALL(cSTIR_convertListmodeToSinograms(lm2s));
+
+		break;
+	}
+
+	deleteDataHandle(lm2s);
+
+	return 0;
+}
