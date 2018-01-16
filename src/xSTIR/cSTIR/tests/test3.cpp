5fbeaf5865c6012eacc5b97c0931483c64a03bf3
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2018-01-16 10:52:59 +0000

    added C tests for PETAcquisitionSensitivityModel

--- src/xSTIR/cSTIR/tests/test3.cpp
+++ src/xSTIR/cSTIR/tests/test3.cpp
@@ -3,6 +3,7 @@
 
 //#include "stir/listmode/LmToProjData.h"
 
+#include "cstir.h"
 #include "object.h"
 #include "stir_types.h"
 #include "stir_data_containers.h"
@@ -79,10 +80,72 @@ int test3b()
 		std::string input_filename =
 			"C:/Users/wps46139/Documents/GitHub/SIRF/data/examples/PET/norm.n.hdr.STIR";
 		//"C:/Users/wps46139/Documents/GitHub/SIRF/data/examples/PET/list.l.hdr.STIR";
-		shared_ptr<BinNormalisationFromECAT8>
-			sptr_n(new BinNormalisationFromECAT8(input_filename));
+		void* sm = 0;
+		for (;;) {
+			void* h = charDataHandle(input_filename.c_str());
+			HANDLE(sm, cSTIR_createPETAcquisitionSensitivityModel(h, "n"));
+			break;
+		}
+		deleteDataHandle(sm);
+		//shared_ptr<PETAcquisitionSensitivityModel> 
+		//	sptr(new PETAcquisitionSensitivityModel(input_filename));
+		//shared_ptr<BinNormalisationFromECAT8>
+		//	sptr_n(new BinNormalisationFromECAT8(input_filename));
 		//shared_ptr<CListModeData> lm_data_ptr;
 		//lm_data_ptr = stir::read_from_file<CListModeData>(input_filename);
+	}
+	catch (...)
+	{
+	}
+	return 0;
+}
+
+int test3c()
+{
+	try {
+		std::string filename ="mu_map.hv";
+		void* image = 0;
+		void* sm = 0;
+		for (;;) {
+			HANDLE(image, cSTIR_objectFromFile("Image", filename.c_str()));
+			HANDLE(sm, cSTIR_createPETAcquisitionSensitivityModel(image, "i"));
+			break;
+		}
+		deleteDataHandle(image);
+		deleteDataHandle(sm);
+		//shared_ptr<PETImageData> sptr_img(new PETImageData(filename));
+		//shared_ptr<PETAcquisitionSensitivityModel>
+		//	sptr(new PETAcquisitionSensitivityModel(*sptr_img));
+		//shared_ptr<BinNormalisationFromAttenuationImage>
+		//	sptr_n(new BinNormalisationFromAttenuationImage(sptr_img->data_sptr()));
+	}
+	catch (...)
+	{
+	}
+	return 0;
+}
+
+int test3d()
+{
+	try {
+		void* ad = 0;
+		void* sm = 0;
+		for (;;) {
+			int span = 1;
+			int max_ring_diff = -1;
+			int view_mash_factor = 1;
+			std::cout << "creating acquisition data...";
+			HANDLE(ad, cSTIR_acquisitionsDataFromScannerInfo
+				("Siemens_mMR", span, max_ring_diff, view_mash_factor));
+			std::cout << "ok\n";
+			CALL(cSTIR_fillAcquisitionsData(ad, 1.0f));
+			std::cout << "creating acquisition sensitivity data...";
+			HANDLE(sm, cSTIR_createPETAcquisitionSensitivityModel(ad, "s"));
+			std::cout << "ok\n";
+			break;
+		}
+		deleteDataHandle(ad);
+		deleteDataHandle(sm);
 	}
 	catch (...)
 	{
