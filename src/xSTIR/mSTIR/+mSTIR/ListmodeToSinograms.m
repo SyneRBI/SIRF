7057f6826da8fcdc49d85ed1e3055d74b07b52f6
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2018-02-05 12:18:14 +0000

    synchronized mSTIR.ListmodeToSinograms with pSTIR

--- src/xSTIR/mSTIR/+mSTIR/ListmodeToSinograms.m
+++ src/xSTIR/mSTIR/+mSTIR/ListmodeToSinograms.m
@@ -21,6 +21,7 @@ classdef ListmodeToSinograms < handle
     properties
         handle_
         name_
+        output_
     end
     methods
         function self = ListmodeToSinograms(file)
@@ -39,32 +40,68 @@ classdef ListmodeToSinograms < handle
             end
         end
         function set_input(self, file)
+            % Sets the listmode file name.
             mSTIR.setParameter(self.handle_, self.name_, 'input', file, 'c')
         end
-        function set_output(self, file)
+        function set_output_prefix(self, file)
+            % Sets the sinograms file names prefix.
             mSTIR.setParameter(self.handle_, self.name_, 'output', file, 'c')
         end
         function set_template(self, file)
+            % Sets the sinograms template.
             mSTIR.setParameter(self.handle_, self.name_, 'template', file, 'c')
         end
-        function set_interval(self, start, stop)
+        function set_time_interval(self, start, stop)
+            % Sets time interval.
+            % Only data scanned during this time interval will be converted.
             ptr = libpointer('singlePtr', [start stop]);
             h = calllib('mstir', 'mSTIR_setListmodeToSinogramsInterval', ...
                 self.handle_, ptr);
             mUtilities.check_status([self.name_ ':set_interval'], h);
             mUtilities.delete(h)
         end
+        function flag_on(self, flag)
+            % Switches on a conversion flag.
+            h = calllib('mstir', 'mSTIR_setListmodeToSinogramsFlag', ...
+                self.handle_, flag, 1);
+            mUtilities.check_status([self.name_ ':flag_on'], h);
+            mUtilities.delete(h)
+        end
+        function flag_off(self, flag)
+            % Switches off a conversion flag.
+            h = calllib('mstir', 'mSTIR_setListmodeToSinogramsFlag', ...
+                self.handle_, flag, 0);
+            mUtilities.check_status([self.name_ ':flag_on'], h);
+            mUtilities.delete(h)
+        end
         function set_up(self)
+            % Sets up the conversion.
             h = calllib('mstir', 'mSTIR_setupListmodeToSinogramsConverter', ...
                 self.handle_);
             mUtilities.check_status([self.name_ ':set_up'], h);
             mUtilities.delete(h)
         end
         function process(self)
-            h = calllib('mstir', 'mSTIR_convertListmodeToSinograms', ...
+            % Performs the conversion.
+            self.output_ = mSTIR.AcquisitionData();
+            self.output_.handle_ = calllib...
+                ('mstir', 'mSTIR_convertListmodeToSinograms', ...
                 self.handle_);
-            mUtilities.check_status([self.name_ ':set_up'], h);
-            mUtilities.delete(h)
+            mUtilities.check_status...
+                ([self.name_ ':process'], self.output_.handle_);
+        end
+        function output = get_output(self)
+            % Returns the sinograms.
+            assert(~isempty(self.output_), 'Conversion to sinograms not done')
+            output = self.output_;
+        end
+        function randoms = estimate_randoms(self)
+            % Estimates randoms.
+            randoms = mSTIR.AcquisitionData();
+            randoms.handle_ = calllib('mstir', 'mSTIR_computeRandoms', ...
+                self.handle_);
+            mUtilities.check_status...
+                ([self.name_ ':estimate_randoms'], randoms.handle_);
         end
     end
 end
