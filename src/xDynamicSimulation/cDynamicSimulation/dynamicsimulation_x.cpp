25fe351464f850253bd90c7897541e9123193393
Johannes Mayer <johannes.mayer@ptb.de>
2018-03-15 12:25:00 +0000

    Able to use classes from the gadgetron_x.* files

--- src/xDynamicSimulation/cDynamicSimulation/dynamicsimulation_x.cpp
+++ src/xDynamicSimulation/cDynamicSimulation/dynamicsimulation_x.cpp
@@ -11,7 +11,11 @@ date: 15. March 2018
 
 void foo( void )
 {
+	
+	ImagesReconstructor ir;		
+	const char* nameOfClass = ir.class_name();
 
 	fprintf(stdout, "This function is called from the cdynamicsimulation library.\n");
+	fprintf(stdout, "It created a class called %s from gadgetron_x.h.\n", nameOfClass);
 
 }
