e95f0afa4f4f442eb1f705887aa6a5fb0ea9fdce
Johannes Mayer <johannes.mayer@ptb.de>
2018-03-21 17:21:05 +0000

    Wrote function to parse  xml files for TissueParemters-> testjob still fails
    Continue tomorrow and see why the filestream won't open. this has
    to be an issue with the filepath and how I typed it.

--- src/xDynamicSimulation/cDynamicSimulation/tissueparameters.cpp
+++ src/xDynamicSimulation/cDynamicSimulation/tissueparameters.cpp
@@ -6,15 +6,40 @@ institute	PTB Berlin
 
 */
 
-
+#include <stdio.h>
+#include <iostream>
 #include "tissueparameters.h"
 
 
 TissueParameterList read_TissueParameters_from_xml(std::string const xml_filepath)
-{
+{	
+	std::cout << xml_filepath << std::endl;
 	std::ifstream in(xml_filepath);
 
+	if ( in.is_open() )
+		std::cout<< "file was opened successfully" <<std::endl;
+	else
+		std::cout<< "failed to open " << xml_filepath << std::endl;
+
+	using boost::property_tree::ptree;
+	ptree pt;
+
+	read_xml( in, pt);	
+
+
 	TissueParameterList tiss_list;
+
+	BOOST_FOREACH( ptree::value_type const& v, pt.get_child("TissueParameter") )
+	{
+		if( v.first == "TissueParameter")		
+		{
+			TissueParameter tiss_par;
+			tiss_par.label_ = v.second.get< unsigned >( "label");
+			tiss_par.name_  = v.second.get< std::string > ("name");
+
+			tiss_list.push_back(tiss_par);
+		}
+	}
 
 	return tiss_list;
 }
