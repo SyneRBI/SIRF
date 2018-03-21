a3974b46d5e8b14530c3c1b7f2c6a9a3465b5d72
Johannes Mayer <johannes.mayer@ptb.de>
2018-03-21 14:28:38 +0000

    Added Tissue Parameter Structs
    Simplistic data containers to save information coming from an xml
    file were added.

---
+++ src/xDynamicSimulation/cDynamicSimulation/tissueparameters.cpp
@@ -0,0 +1,14 @@
+/* Implementation of TissueParameter
+
+author		johannes mayer
+date		20180321
+institute	PTB Berlin
+
+*/
+
+
+#include "tissueparameters.h"
+
+
+
+
---
+++ src/xDynamicSimulation/cDynamicSimulation/tissueparameters.h
@@ -0,0 +1,45 @@
+/* file describing tissue parameter structs. 
+these data containers should be filled by an xml parser.
+
+author		johannes mayer
+date		20180321
+institute	PTB Berlin
+
+*/
+
+struct TissueParameter 
+{
+
+	int label_;
+	char* name_;
+
+};
+
+
+struct MRTissueParameter
+{
+
+	float t1_miliseconds_;
+	float t2_miliseconds_;
+	float cs_ppm_;
+
+};
+
+
+
+struct PETTissueParameter
+{
+
+	float attenuation_1_by_mm_;
+	float suv_;
+};
+
+
+
+
+
+
+
+
+
+
