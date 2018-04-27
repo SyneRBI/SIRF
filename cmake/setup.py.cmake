0e64f56e1726fac6032216a8fba5603e4e6735ca
Casper da Costa-Luis <casper.dcl@physics.org>
2018-04-27 15:58:35 +0100

    directory restructure

--- cmake/setup.py.cmake
+++ cmake/setup.py.cmake
@@ -6,7 +6,7 @@ if(PYTHONINTERP_FOUND)
   function(python_pkg_alias PY_PKG_NEW PY_PKG_OLD)
     list(APPEND PYTHON_SETUP_PKGS ${PY_PKG_NEW})
     set(PYTHON_SETUP_PKGS "${PYTHON_SETUP_PKGS}" PARENT_SCOPE)
-    set(SETUP_PY_INIT_IN "${CMAKE_CURRENT_SOURCE_DIR}/cmake/__init__.py.in")
+    set(SETUP_PY_INIT_IN "${CMAKE_CURRENT_LIST_DIR}/__init__.py.in")
     set(SETUP_PY_INIT "${PYTHON_DEST}/${PY_PKG_NEW}/__init__.py")
     configure_file("${SETUP_PY_INIT_IN}" "${SETUP_PY_INIT}")
     # message(STATUS "setup.py:${SETUP_PY_INIT}")
@@ -24,7 +24,7 @@ if(PYTHONINTERP_FOUND)
   # message(STATUS "setup.py:pacakges:${PYTHON_SETUP_PKGS_CSV}")
 
   # Create setup.py
-  set(SETUP_PY_IN "${CMAKE_CURRENT_SOURCE_DIR}/cmake/setup.py.in")
+  set(SETUP_PY_IN "${CMAKE_CURRENT_LIST_DIR}/setup.py.in")
   set(SETUP_PY "${PYTHON_DEST}/setup.py")
   set(SETUP_PY_INIT "${PYTHON_DEST}/sirf/__init__.py")
   message(STATUS "setup.py:${SETUP_PY}")
@@ -38,7 +38,7 @@ if(PYTHONINTERP_FOUND)
     DEPENDS "${SETUP_PY_IN}"
     WORKING_DIRECTORY "${PYTHON_DEST}")
 
-  add_custom_target(pybuild_sirf ALL DEPENDS ${SETUP_PY_INIT})
+  add_custom_target(pybuild_sirf ALL DEPENDS "${SETUP_PY_INIT}")
 
   # python setup.py install
   if("${PYTHON_STRATEGY}" STREQUAL "SETUP_PY")
