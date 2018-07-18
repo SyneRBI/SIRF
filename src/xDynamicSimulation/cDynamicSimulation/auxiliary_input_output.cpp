71e94427f5bc0e5909828ecfde0936b662f526de
johannesmayer <johannes.mayer@ptb.de>
2018-07-18 14:50:04 +0000

    Added correct acquis. info to acquisitions vector in reader

--- src/xDynamicSimulation/cDynamicSimulation/auxiliary_input_output.cpp
+++ src/xDynamicSimulation/cDynamicSimulation/auxiliary_input_output.cpp
@@ -33,25 +33,29 @@ ISMRMRD::IsmrmrdHeader mr_io::read_ismrmrd_header( std::string path_ismrmrd_h5_f
 AcquisitionsVector mr_io::read_ismrmrd_acquisitions( std::string path_ismrmrd_h5_file_with_ext )
 {
 
+	ISMRMRD::IsmrmrdHeader hdr = mr_io::read_ismrmrd_header( path_ismrmrd_h5_file_with_ext );
+	std::ostringstream out;
+	serialize(hdr, out);
+
 	std::cout<< "Started reading acquisitions from " << path_ismrmrd_h5_file_with_ext << std::endl;
 
 	ISMRMRD::Dataset d(path_ismrmrd_h5_file_with_ext.c_str(),"dataset", false);
 
 	uint32_t num_acquis = d.getNumberOfAcquisitions();
 
-	AcquisitionsVector acqu_vect;
+	AcquisitionsVector acq_vec(out.str());
 	ISMRMRD::Acquisition acq;
 
 	for( uint32_t i_acqu=1; i_acqu<num_acquis; i_acqu++)
 	{
 		d.readAcquisition( i_acqu, acq);
-		acqu_vect.append_acquisition( acq );
+		acq_vec.append_acquisition( acq );
 
 	}
 
 	std::cout<< "Finished reading acquisitions from " << path_ismrmrd_h5_file_with_ext << std::endl;
 
-	return acqu_vect;
+	return acq_vec;
 
 }
 
