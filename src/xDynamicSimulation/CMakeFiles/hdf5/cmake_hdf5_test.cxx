d4b3b64a78d3d3699aaf1af94e47a7cca75aaf29
johannesmayer <johannes.mayer@ptb.de>
2018-07-31 13:17:26 +0000

    removed CMake output

--- src/xDynamicSimulation/CMakeFiles/hdf5/cmake_hdf5_test.cxx
+++
@@ -1,9 +0,0 @@
-#include <H5Cpp.h>
-#ifndef H5_NO_NAMESPACE
-using namespace H5;
-#endif
-int main(int argc, char **argv) {
-  char const* info_ver = "INFO" ":" H5_VERSION;
-  H5File file("foo.h5", H5F_ACC_TRUNC);
-  return 0;
-}
