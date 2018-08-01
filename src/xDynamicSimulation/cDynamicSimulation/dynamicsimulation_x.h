2fe1fcb548257c5806586d9a5c0394c994403523
johannesmayer <johannes.mayer@ptb.de>
2018-08-01 15:16:18 +0000

    Started Writing PETDynamicSimulation

--- src/xDynamicSimulation/cDynamicSimulation/dynamicsimulation_x.h
+++ src/xDynamicSimulation/cDynamicSimulation/dynamicsimulation_x.h
@@ -93,39 +93,18 @@ private:
 
 };
 
-/*
 
-class MRDynamicSimulation {
-
+/*class PETDynamicSimulation : public aDynamicSimulation{
 
 public:
+	PETDynamicSimulation( PETContrastGenerator pet_cont_gen) : pet_cont_gen_(pet_cont_gen);
 
-	MRDynamicSimulation( MRContrastGenerator mr_cont_gen) : mr_cont_gen_( mr_cont_gen ) { };
+	void simulate_dynamics( void ){};
+
+private:
+
+	PETContrastGenerator pet_cont_gen_;
 	
-	void set_filename_mr_rawdata( std::string const filename_ismrmrd_src_file ) { this->filename_mr_rawdata_ = filename_ismrmrd_src_file; };
-	std::string get_filename_mr_rawdata( void )	{ return this-> filename_mr_rawdata_; };
-
-	ISMRMRD::IsmrmrdHeader get_ismrmrd_header( void ){ return this->hdr_;};
-
-	void simulate_dynamics( void );
-
-	void write_simulation_results( std::string const filename_output_with_h5_extension );
-
-	void extract_src_information( void );
-
-protected:
-	
-	std::string filename_mr_rawdata_;
-	ISMRMRD::IsmrmrdHeader hdr_;
-
-	AcquisitionsVector source_acquisitions_;
-	AcquisitionsVector target_acquisitions_;
-	
-	MRContrastGenerator mr_cont_gen_;
-	MRAcquisitionModel acq_model_;
-
-
 };
-
-
 */
+
