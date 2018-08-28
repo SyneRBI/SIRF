02f1418b6d32cf7223fd8fe11f42c666d4e62e70
johannesmayer <johannes.mayer@ptb.de>
2018-08-28 09:29:53 +0000

    Fix bug in testjob for dynsim deformer

--- src/xDynamicSimulation/cDynamicSimulation/Testing/tests_dynsim_deformer.cpp
+++ src/xDynamicSimulation/cDynamicSimulation/Testing/tests_dynsim_deformer.cpp
@@ -18,14 +18,17 @@ try
 	{
 		bool test_succesful = true;
 
-		SIRFImageDataDeformation img_dat_deform;
-
 		ISMRMRD::NDArray< unsigned int > segmentation_labels = read_segmentation_from_h5( H5_XCAT_PHANTOM_PATH );
+
 		MRContrastGenerator mr_cont_gen( segmentation_labels, XML_XCAT_PATH);
+
+		ISMRMRD::IsmrmrdHeader hdr = mr_io::read_ismrmrd_header(ISMRMRD_H5_TEST_PATH);
+		mr_cont_gen.set_rawdata_header(hdr);
 
 		mr_cont_gen.map_contrast();
 
-		// DynamicSimulationDeformer::deform_contrast_generator(mr_cont_gen, img_dat_deform);
+		SIRFImageDataDeformation img_dat_deform;
+		DynamicSimulationDeformer::deform_contrast_generator(mr_cont_gen, img_dat_deform);
 
 		test_succesful = false;
 
