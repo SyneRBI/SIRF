7db1386b61c32674875e9328785f23b3a59b5bbd
johannesmayer <johannes.mayer@ptb.de>
2018-09-18 09:40:12 +0000

    Add volume reorientation class

---
+++ src/xDynamicSimulation/cDynamicSimulation/Testing/tests_volume_orientator.h
@@ -0,0 +1,30 @@
+/* ================================================
+
+Author: Johannes Mayer
+Date: 2018.09.18
+Institution: Physikalisch-Technische Bundesanstalt Berlin
+
+================================================ */
+
+#pragma once
+
+#include "volume_orientator.h"
+
+
+
+namespace sirf{
+
+
+
+
+class aVolumeOrientatorTester{
+
+
+public:
+	static bool test_reorient_image( void );
+
+
+};
+
+
+}
