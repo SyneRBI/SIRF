e3385bb9791e8b0d9b5639834caea187ae2a1854
johannesmayer <johannes.mayer@ptb.de>
2018-10-25 10:02:58 +0000

    Acquisition reader ignoring noise calib

--- src/xDynamicSimulation/cDynamicSimulation/auxiliary_input_output.cpp
+++ src/xDynamicSimulation/cDynamicSimulation/auxiliary_input_output.cpp
@@ -45,10 +45,17 @@ AcquisitionsVector mr_io::read_ismrmrd_acquisitions( std::string path_ismrmrd_h5
 	uint32_t num_acquis = d.getNumberOfAcquisitions();
 
 	AcquisitionsVector acq_vec(out.str());
-	ISMRMRD::Acquisition acq;
 
 	for( uint32_t i_acqu=0; i_acqu<num_acquis; i_acqu++)
 	{
+		ISMRMRD::Acquisition acq;
+		if (acq).isFlagSet(ISMRMRD::ISMRMRD_ACQ_IS_NOISE_MEASUREMENT)
+		{
+			std::cout << "Ignoring acquisition # " << i_acqu <<" due to it being noise calibration."
+			continue;
+		}
+
+
 		if ((i_acqu%1000) == 0 )
 			std::cout << float(i_acqu)/num_acquis*100.f << " % " << std::endl;
 
