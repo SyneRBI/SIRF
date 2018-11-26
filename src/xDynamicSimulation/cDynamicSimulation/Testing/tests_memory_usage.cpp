e7b21bd5c10396086002be51adaaa488a6b0cbc4
johannesmayer <johannes.mayer@ptb.de>
2018-11-26 12:20:01 +0000

    add memory usage testjobs

---
+++ src/xDynamicSimulation/cDynamicSimulation/Testing/tests_memory_usage.cpp
@@ -0,0 +1,29 @@
+/* ================================================
+
+Author: Johannes Mayer
+Date: 2018.11.26
+Institution: Physikalisch-Technische Bundesanstalt Berlin
+
+================================================ */
+
+#include "tests_memory_usage.h"
+
+
+
+bool tests_memory::test_acquisition_vector_memory( void )
+{
+	try
+	{
+		int const num_instances_to_construct;	
+		
+
+		return true;
+	}
+	catch( std::runtime_error const &e)
+	{	
+		std::cout << "Exception caught " <<__FUNCTION__ <<" .!" <<std::endl;
+		std::cout << e.what() << std::endl;
+		throw e;
+	}
+}
+
---
+++ src/xDynamicSimulation/cDynamicSimulation/Testing/tests_memory_usage.h
@@ -0,0 +1,22 @@
+/* ================================================
+
+Author: Johannes Mayer
+Date: 2018.11.26
+Institution: Physikalisch-Technische Bundesanstalt Berlin
+
+================================================ */
+
+
+#pragma once
+
+
+
+
+#include "gadgetron_data_containers.h"
+
+
+namespace tests_memory{
+
+bool test_acquisition_vector_memory( void );
+
+}
