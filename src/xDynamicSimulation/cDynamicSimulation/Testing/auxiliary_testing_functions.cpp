bb635b5368a2753305360da3cbd6e9eaab5267e2
johannesmayer <johannes.mayer@ptb.de>
2018-12-07 14:56:36 +0000

    Updated signal mocks to sptr acquisition use

--- src/xDynamicSimulation/cDynamicSimulation/Testing/auxiliary_testing_functions.cpp
+++ src/xDynamicSimulation/cDynamicSimulation/Testing/auxiliary_testing_functions.cpp
@@ -450,7 +450,7 @@ ISMRMRD::NDArray< complex_float_t > aux_test::get_mock_csm( void )
 ISMRMRD::Image<complex_float_t> aux_test::get_mock_gaussian_csm( std::vector<size_t> vol_dims, int const num_coils )
 {
 
-	int const num_non_zero_coils = std::floor( log2( num_coils ) );
+	int const num_non_zero_coils = pow( (double)2,  std::floor( log2( num_coils ) ) );
 
 	std::vector<float> sensitivity_widths {vol_dims[0] /2.f, vol_dims[1] /2.f, vol_dims[2] /2.f };
 
@@ -731,12 +731,11 @@ SignalContainer aux_test::get_mock_sinus_signal( AcquisitionsVector &acq_vec, Ti
 
 	unsigned const num_sampling_points = acq_vec.number();
 	std::vector< TimeAxisType > all_time_points;
+
 	for(size_t ia=0; ia<num_sampling_points; ia++)
 	{
-		ISMRMRD::Acquisition acq;
-		acq_vec.get_acquisition(ia, acq);
-
-		all_time_points.push_back(acq.getHead().acquisition_time_stamp); 	
+		auto sptr_acq = acq_vec.get_acquisition_sptr(ia);
+		all_time_points.push_back(sptr_acq->getHead().acquisition_time_stamp); 	
 	}
 
 	auto minmax_it = std::minmax_element(std::begin(all_time_points), std::end(all_time_points));
@@ -759,13 +758,13 @@ SignalContainer aux_test::get_mock_sinus_signal( AcquisitionsVector &acq_vec, Ti
 
 SignalContainer aux_test::get_mock_sawtooth_signal( AcquisitionsVector acq_vec, TimeAxisType const period_duration_ms)
 {
-	ISMRMRD::Acquisition acq;
 	
-	acq_vec.get_acquisition(0, acq);
-	TimeAxisType t_0 = acq.getHead().acquisition_time_stamp;
+	auto sptr_acq = acq_vec.get_acquisition_sptr(0);
+	TimeAxisType t_0 = sptr_acq->getHead().acquisition_time_stamp;
 			
-	acq_vec.get_acquisition(acq_vec.items()-1, acq);
-	TimeAxisType t_fin = acq.getHead().acquisition_time_stamp;
+
+	sptr_acq = acq_vec.get_acquisition_sptr(acq_vec.items()-1);
+	TimeAxisType t_fin = sptr_acq->getHead().acquisition_time_stamp;
 
 
 	unsigned const num_sampling_points = acq_vec.number();
