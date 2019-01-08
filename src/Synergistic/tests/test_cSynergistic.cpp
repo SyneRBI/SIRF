f96daab05c236a6c0dde4824d7cd2fa67badb5b0
Richard Brown <richard.brown@ucl.ac.uk>
2019-01-08 17:08:39 +0000

    test if we can do STIRImageData->NiftiImageData3D

--- src/Synergistic/tests/test_cSynergistic.cpp
+++ src/Synergistic/tests/test_cSynergistic.cpp
@@ -29,8 +29,25 @@ limitations under the License.
 
 #include "sirf/cSTIR/stir_data_containers.h"
 #include "sirf/cReg/NiftiImageData3D.h"
+#include "sirf/cReg/SIRFRegNiftyResample.h"
 
 using namespace sirf;
+
+static void create_stir_output_file_format(const std::string &path)
+{
+    std::ofstream file(path);
+    if (!file.is_open())
+        throw std::runtime_error("Unable to write stir output file format.");
+    file << "OutputFileFormat Parameters:=\n";
+    file << "output file format type := ITK\n";
+    file << "ITK Output File Format Parameters:=\n";
+    file << "number format := float\n";
+    file << "number_of_bytes_per_pixel:=4\n";
+    file << "default extension:=.nii\n";
+    file << "End ITK Output File Format Parameters:=\n";
+    file << "End:=\n";
+    file.close();
+}
 
 int main(int argc, char* argv[])
 {
@@ -42,19 +59,23 @@ int main(int argc, char* argv[])
         else         SIRF_PATH = argv[1];
 
         // Input filenames
-        const std::string nifti_filename = SIRF_PATH + "/data/examples/Registration/test.nii.gz";
+        const std::string nifti_filename = SIRF_PATH + "/data/examples/Registration/test2.nii.gz";
+
+        // Load the image as a NiftiImageData3D
+        NiftiImageData3D<float> image_nifti(nifti_filename);
 
         // Read as STIRImageData, convert NiftiImageData3D and save to file
         STIRImageData image_stir(nifti_filename);
         NiftiImageData3D<float> image_nifti_from_stir(image_stir);
-        image_nifti_from_stir.write("results/stir_to_nifti.nii");
-
-        // Load the original image as a NiftiImageData3D
-        NiftiImageData3D<float> image_nifti(nifti_filename);
+        image_nifti_from_stir.write("results/stir_to_nifti.nii",image_nifti.get_original_datatype());
 
         // Compare the two
         if (image_nifti != image_nifti_from_stir)
             throw std::runtime_error("Conversion from STIR to Nifti failed");
+
+        // Also save the STIRImageData to file (might be useful visual for comparison)
+        create_stir_output_file_format("results/stir_output_file_format_nifti.par");
+        image_stir.write("results/stir.nii","results/stir_output_file_format_nifti.par");
 
     // Error handling
     } catch(const std::exception &error) {
