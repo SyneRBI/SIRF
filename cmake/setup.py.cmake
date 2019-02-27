405b628f454bf5f5b1029c8c6f99efa1baa0a94e
Casper da Costa-Luis <casper.dcl@physics.org>
2019-02-27 15:02:07 +0000

    a real fix

--- cmake/setup.py.cmake
+++ cmake/setup.py.cmake
@@ -2,19 +2,17 @@
 if(BUILD_PYTHON)
   set(PYTHON_SETUP_PKGS "sirf" CACHE INTERNAL "list of provided python packages")
 
-  install(CODE "execute_process(COMMAND\n\
-    \"${CMAKE_COMMAND}\" -E make_directory \"${PYTHON_DEST}/sirf\")")
   # alias sirf.p* -> p* for backward-compatibility
   function(python_pkg_alias PY_PKG_NEW PY_PKG_OLD)
     list(APPEND PYTHON_SETUP_PKGS ${PY_PKG_NEW})
     set(PYTHON_SETUP_PKGS "${PYTHON_SETUP_PKGS}" PARENT_SCOPE)
-    set(SETUP_PY_INIT_IN "${CMAKE_CURRENT_LIST_DIR}/__init__.py.in")
-    set(SETUP_PY_INIT "${CMAKE_CURRENT_BINARY_DIR}/cmake/${PY_PKG_NEW}/__init__.py")
-    configure_file("${SETUP_PY_INIT_IN}" "${SETUP_PY_INIT}")
-    install(FILES "${SETUP_PY_INIT}"
+    set(PY_PKG_INIT_IN "${CMAKE_CURRENT_LIST_DIR}/__init__.py.in")
+    set(PY_PKG_INIT "${CMAKE_CURRENT_BINARY_DIR}/cmake/${PY_PKG_NEW}/__init__.py")
+    configure_file("${PY_PKG_INIT_IN}" "${PY_PKG_INIT}")  # depends: PY_PKG_OLD
+    install(FILES "${PY_PKG_INIT}"
       DESTINATION "${PYTHON_DEST}/${PY_PKG_NEW}")
-    # message(STATUS "setup.py:${SETUP_PY_INIT}")
-    message(STATUS "setup.py:${PY_PKG_NEW}<-${PY_PKG_OLD}")
+    # message(STATUS "__init__.py:${PY_INIT}")
+    message(STATUS "python alias:${PY_PKG_NEW}<-${PY_PKG_OLD}")
   endfunction(python_pkg_alias)
   python_pkg_alias(pGadgetron "sirf.Gadgetron")
   python_pkg_alias(pSTIR "sirf.STIR")
@@ -42,11 +40,16 @@ if(BUILD_PYTHON)
     add_custom_command(OUTPUT "${SETUP_PY_INIT}"
       COMMAND "${CMAKE_COMMAND}" -E make_directory "${PYTHON_DEST}/sirf"
       COMMAND "${CMAKE_COMMAND}" -E touch "${SETUP_PY_INIT}"
-      # COMMAND "${PYTHON_EXECUTABLE}" setup.py build
-      # DEPENDS "${PYTHON_DEST}/setup.py"
+      COMMAND "${PYTHON_EXECUTABLE}" setup.py build
+      DEPENDS "${PYTHON_DEST}/setup.py"
       WORKING_DIRECTORY "${PYTHON_DEST}")
-
-    add_custom_target(pybuild_sirf ALL DEPENDS "${SETUP_PY_INIT}")
+    #add_custom_target(pybuild_sirf ALL DEPENDS "${SETUP_PY_INIT}")
+    # cannot do above (#179 -> https://github.com/CCPPETMR/SIRF/pull/308)
+    install(CODE "execute_process(\n\
+      COMMAND\n\
+        \"${CMAKE_COMMAND}\" -E make_directory \"${PYTHON_DEST}/sirf\"\n\
+      COMMAND\n\
+        \"${CMAKE_COMMAND}\" -E touch \"${SETUP_PY_INIT}\")")
 
     # python setup.py install
     if("${PYTHON_STRATEGY}" STREQUAL "SETUP_PY")
