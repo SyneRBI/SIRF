17776a96674adcd01368af669d5947d85cf7a705
Richard Brown <richard.brown@ucl.ac.uk>
2019-04-08 15:13:29 +0100

    Fix synergistic copyright

--- src/Synergistic/sirf_registration.cpp
+++ src/Synergistic/sirf_registration.cpp
@@ -1,6 +1,6 @@
 /*
 CCP PETMR Synergistic Image Reconstruction Framework (SIRF)
-Copyright 2018-2019 Rutherford Appleton Laboratory STFC
+Copyright 2018-2019 University College London
 
 This is software developed for the Collaborative Computational
 Project in Positron Emission Tomography and Magnetic Resonance imaging
