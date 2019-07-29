8f3131cf24805ffa4fc5e10fe630cb57b4749f69
Johannes Mayer <johannes.mayer@ptb.de>
2019-07-29 09:53:04 +0100

    included 5d testjobs in program to run

--- src/xDynamicSimulation/cDynamicSimulation/Testing/all_simulation_tests.cpp
+++ src/xDynamicSimulation/cDynamicSimulation/Testing/all_simulation_tests.cpp
@@ -161,7 +161,10 @@ void run_tests_dynamic_simulation( void )
 	// mr_dynsim_tests.push_back(tests_mr_dynsim::test_dce_acquisition());
 
 	std::cout << "MR 7 ----------------------------------------------------" <<std::endl;
-	mr_dynsim_tests.push_back(tests_mr_dynsim::test_4d_mri_acquisition());
+	// mr_dynsim_tests.push_back(tests_mr_dynsim::test_4d_mri_acquisition());
+
+	std::cout << "MR 8 ----------------------------------------------------" <<std::endl;
+	mr_dynsim_tests.push_back(tests_mr_dynsim::test_5d_mri_acquisition());
 
 	
 	std::cout << "mr dynamic simulation test results = ";
@@ -190,6 +193,9 @@ void run_tests_dynamic_simulation( void )
 	std::cout << "PET 5 ----------------------------------------------------" <<std::endl;
 	// pet_dynsim_tests.push_back(test_pet_dynsim::test_4d_pet_acquisition());
 
+	std::cout << "PET 6 ----------------------------------------------------" <<std::endl;
+	// pet_dynsim_tests.push_back(test_pet_dynsim::test_5d_pet_acquisition());
+	
 
 	std::cout << "pet dynamic simulation test results = ";
 	for( size_t i=0; i<pet_dynsim_tests.size(); i++)
