1c5705b85312d43d8be02778a2e3c1fefe587c5a
johannesmayer <johannes.mayer@ptb.de>
2020-02-04 14:07:50 +0000

    Added tests for cpp level of xGadgetron

---
+++ src/xGadgetron/cGadgetron/tests/mrtests.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+//#include "sirf/common/DataContainer.h"
+#include "sirf/Gadgetron/gadgetron_data_containers.h"
+
+using namespace sirf;
+
+int main ()
+{
+	try{
+        std::cout << "Hello Tests." << std::endl;
+        return 0;
+	}
+	catch(...)
+	{
+
+	}
+}
