bf35aca8ebed659f65c4a5bd057d62c0fbe8ab6e
Edoardo Pasca <edo.paskino@gmail.com>
2020-02-27 11:47:29 +0000

    fixed PET recon

--- examples/Python/PETMR/PETMR_MCIR.py
+++ examples/Python/PETMR/PETMR_MCIR.py
@@ -16,6 +16,9 @@ import os
 import glob
 import numpy as np
 
+import subprocess
+import fileinput
+
 import matplotlib.pyplot as plt
 
 pet.AcquisitionData.set_storage_scheme('memory')
@@ -75,6 +78,37 @@ def convert_nifti_to_stir_ImageData(image, templ_sino, dim):
     out.fill(image.as_array())
     return out
 
+
+
+def read_2D_STIR_nii(filename):
+    temp_filename = filename + "_temp.hv"
+    a = pet.ImageData(filename)
+    a.write(temp_filename)
+    text_to_search = "scaling factor (mm/pixel) [3] := 1"
+    text_to_replace = "scaling factor (mm/pixel) [3] := 2.03125"
+    dfile = fileinput.FileInput(temp_filename, inplace=True, backup='.bak')
+    for line in dfile:
+        print(line.replace(text_to_search, text_to_replace))
+    dfile.close()
+    im = pet.ImageData(temp_filename)
+    return im
+
+def add_noise(counts, sinogram):
+    sino_arr = sinogram.as_array()
+    minmax = (sino_arr.min(), sino_arr.max())
+    if counts > 0 and counts <= 1:
+        counts = counts * (minmax[1] - minmax[0])
+    elif isinstance (counts, int):
+        pass
+       
+    sino_arr = counts * ((sino_arr -minmax[0]) / (minmax[1]-minmax[0]))
+    noisy_counts = sinogram * 0.
+    noisy_counts.fill( np.random.poisson(sino_arr) )
+    
+    return noisy_counts
+
+
+
 data_path = os.path.join( os.path.abspath('/home/ofn77899') ,
                           'brainweb', 'brainweb_single_slice')
 
@@ -90,26 +124,46 @@ transform_matrices_files  = sorted( glob.glob(os.path.join( data_path, 'fwd_tm*.
 
 # Acquisition Data
 noisy_sinogram_files = sorted( glob.glob(os.path.join (data_path, 'sino_FDG_noisy_mf*.hs' )) )
-
+noiseless_sinogram_files = sorted( glob.glob(os.path.join (data_path, 'sino_FDG_mf*.hs' )) )
 # 
+# needs to go to the data directory
+os.chdir(data_path)
+
+
+#### 
+## Shows the sinograms
+if True:
+    noisysino = pet.AcquisitionData(os.path.basename(noisy_sinogram_files[0])).as_array()
+    sino = pet.AcquisitionData(os.path.basename(noiseless_sinogram_files[0])).as_array()
+    sino_positive = pet.AcquisitionData(os.path.basename(noiseless_sinogram_files[0])).maximum(0).as_array()
+    noisy_positive = add_noise(0.25, pet.AcquisitionData(os.path.basename(noiseless_sinogram_files[0])).maximum(0))
+    ns_minmax = noisysino.min(), noisysino.max()
+    s_minmax = sino.min(), sino.max()
+    sp_minmax = sino_positive.min(), sino_positive.max()
+    plotter2D( [sino[0][0], noisysino[0][0] , sino_positive[0][0], noisy_positive.as_array()[0][0]], 
+    titles=['noiseless sino {}'.format(s_minmax), 'noisy sino {}'.format(ns_minmax), 
+            'noiseless sino positive{}'.format(sp_minmax),
+            'noisy positive 25%'] )
+
 
 rotated_sinos = []
 resamplers = []
 ams = []
 
-# needs to go to the data directory
-os.chdir(data_path)
+
 dim = (1, 150, 150)
 # We'll need a template sinogram
 mmr_template_sino_single_slice = os.path.join( data_path ,'mmr_single_slice_template.hs')
 templ_sino = pet.AcquisitionData(mmr_template_sino_single_slice)
-pet_target_image_template_reg = reg.NiftiImageData(os.path.basename(FDG_files[0]))
-pet_target_image_template = convert_nifti_to_stir_ImageData(pet_target_image_template_reg, templ_sino, dim)
+
+pet_target_image_template = read_2D_STIR_nii(os.path.basename(FDG_files[0]))
 
 for ms in range(n_ms):
     # load the noisy Sinogram
+    sino = pet.AcquisitionData(os.path.basename(noiseless_sinogram_files[ms])).maximum(0)
+    
     rotated_sinos.append( 
-       pet.AcquisitionData(os.path.basename(noisy_sinogram_files[ms]))
+        add_noise(0.25, sino)
     )
     # create the resamplers from the TransformationMatrix
     tm = reg.AffineTransformation(os.path.basename(transform_matrices_files[ms]))
@@ -119,62 +173,79 @@ for ms in range(n_ms):
  
     )
     # create the Acquisition Model with appropriate uMap
-    # because of https://github.com/CCPPETMR/SIRF/issues/562 we 
-    # pass from NumPy
-    uMap = reg.NiftiImageData(os.path.basename(uMap_files[ms]))
-    # im = pet.ImageData(templ_sino)
-    # dim=(1,150,150)
-    # voxel_size=im.voxel_sizes()
-    # im.initialise(dim,voxel_size)
-    # im.fill(uMap.as_array())
-    im = convert_nifti_to_stir_ImageData(uMap, templ_sino, dim)
+    uMap = read_2D_STIR_nii(os.path.basename(uMap_files[ms]))
 
     ams.append(
-        get_acquisition_model(im, templ_sino)
+        get_acquisition_model(uMap, templ_sino)
     )
+
+if False:
+    # plot the solutions to see that with the resampler we get the same orientation
+    implot = []
+    for ms in range(n_ms):
+        img = read_2D_STIR_nii(os.path.basename(FDG_files[ms]))
+        implot.append(img.as_array()[0])
+        implot.append(resamplers[ms].direct(img).as_array()[0])
+
+    plotter2D( implot )
 
 # compose the resampler with the acquisition models
 C = [ CompositionOperator(am, res) for am, res in zip (*(ams, resamplers)) ]
+C = [ am for am in ams ]
+print ("number of motion states", len(resamplers))
 
-# the first AcquisitionModel doesn't need to be resampled we will try to reconstruct in 
-# the orientation of the motion state 0.
-# C[0] = ams[0]
 
 # Configure the PDHG algorithm
 
 kl = [ KullbackLeibler(b=rotated_sino, eta=(rotated_sino * 0 + 1e-5)) for rotated_sino in rotated_sinos ] 
 f = BlockFunction(*kl)
 K = BlockOperator(*C)
-normK = K.norm(iterations=10)
+
+#f = kl[0]
+#K = ams[0]
+
+#normK = K.norm(iterations=10)
+normK = LinearOperator.PowerMethod(K, iterations=10)[0]
 #default values
 sigma = 1/normK
 tau = 1/normK 
-
+sigma = 0.1
+tau = 1/(sigma*normK**2)
 print ("Norm of the BlockOperator ", normK)
 
     
 # TV regularisation
 #regularisation parameters for TV
 # 
-r_alpha = 5e-1
+r_alpha = 5e-2
 r_iterations = 500
 r_tolerance = 1e-7
 r_iso = 0
 r_nonneg = 1
 r_printing = 0
 
-TV = FGP_TV(r_alpha, r_iterations, r_tolerance, r_iso,r_nonneg,r_printing,'cpu')
+TV = FGP_TV(r_alpha, r_iterations, r_tolerance, r_iso,r_nonneg,r_printing,'gpu')
 
 print ("current dir", os.getcwd())
 
 G = IndicatorBox(lower=0)
-
+# G = TV
 pdhg = PDHG(f = f, g = G, operator = K, sigma = sigma, tau = tau, 
             max_iteration = 1000,
             update_objective_interval = 1)
 
 pdhg.run(100, verbose=False)
+#img = convert_nifti_to_stir_ImageData(reg.NiftiImageData(os.path.basename(FDG_files[0])), templ_sino, dim)
+img = read_2D_STIR_nii(os.path.basename(FDG_files[0]))
+solution = resamplers[0].direct(img)
 
-solution = reg.NiftiImageData(os.path.basename(FDG_files[0]))
-plotter2D([solution.as_array(), pdhg.get_output().as_array()[0]] )
+
+res = reg.NiftyResample()
+res.set_reference_image(pdhg.get_output())
+res.set_floating_image(solution)
+res.set_interpolation_type_to_linear()
+
+solution2 = res.direct(solution)
+
+plotter2D([solution.as_array()[0], pdhg.get_output().as_array()[0], solution2.as_array()[0]] )
 # %%
