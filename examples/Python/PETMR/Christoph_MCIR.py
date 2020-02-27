7ed4e178e89b1ff35b8f052c77b2114a9a2ba8b4
Edoardo Pasca <edo.paskino@gmail.com>
2020-02-27 16:34:13 +0000

    use brainweb data

--- examples/Python/PETMR/Christoph_MCIR.py
+++ examples/Python/PETMR/Christoph_MCIR.py
@@ -1,6 +1,8 @@
 import sirf.Gadgetron as pMR
 import sirf.STIR as pet
+import sirf.Reg as reg
 from sirf.Utilities import examples_data_path
+from ccpi.utilities.display import plotter2D
 
 pet.AcquisitionData.set_storage_scheme('memory')
 
@@ -15,6 +17,8 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+import glob
+
 plt.close('all')
 
 # path- and filename of raw data file
@@ -23,15 +27,32 @@ pname = os.path.abspath(examples_data_path('MR'))
 fname = 'grappa2_1rep.h5'
 
 
+data_path = os.path.join( os.path.abspath('/home/ofn77899') ,
+                          'brainweb', 'brainweb_single_slice')
 
+T1_files   = sorted( glob.glob(os.path.join( data_path, 'T1_mf*.nii') ) )
+T2_files   = sorted( glob.glob(os.path.join( data_path, 'T2_mf*.nii') ) )
+transform_matrices_files  = sorted( glob.glob(os.path.join( data_path, 'fwd_tm*.txt') ) )
+for f in T1_files:
+    print (f)
+
+
+# chdir to data dir
+# os.chdir(data_path)
 # Number of motion states
 num_ms = 4
 
 ## Create motion states
 
 # Split k-space data into different motion states
-acq = pMR.AcquisitionData(os.path.join(pname , fname ))
+# acq = pMR.AcquisitionData(os.path.join(data_path , 'CSM_GRAPPA2_48_FOV180.h5' ))
 
+acq = pMR.AcquisitionData(os.path.join(data_path , 'CSM_FULLY_FOV180.h5' ))
+
+# acq = pMR.AcquisitionData(os.path.join(os.path.abspath('/home/ofn77899') ,
+#                           'brainweb' , 'CSM_GRAPPA2_48_FOV180.h5' ) )
+
+# acq = pMR.AcquisitionData(os.path.join(pname , fname ))
 acq.sort_by_time()
 
 # Create interleaved sampling
@@ -71,6 +92,8 @@ recon.compute_gfactors(False)
 recon.process()
 ref_im = recon.get_output()
 
+print ("ref_im shape", ref_im.shape)
+
 fig, ax = plt.subplots(1)
 ax.imshow(np.abs(ref_im.as_array()[0,:,:]))
 
@@ -81,7 +104,15 @@ csm.calculate(prep_data)
 fig, ax = plt.subplots(1,4)
 for ind in range(4):
     ax[ind].imshow(np.abs(csm.as_array()[ind,0,:,:]))
+plt.show()
 
+t1s = []
+for t1f in T1_files:
+    t1s.append(
+        reg.NiftiImageData(t1f).as_array()
+    )
+    print (t1s[-1].shape)
+plotter2D(t1s)
 
 ## Simulate different motion states
 ref_im_ms = ref_im.clone()
@@ -89,10 +120,20 @@ acq_ms_sim = [0]*num_ms
 for ind in range(num_ms):
     AcqMod = pMR.AcquisitionModel(acq_ms[ind], ref_im)
     AcqMod.set_coil_sensitivity_maps(csm)
-
-    cim = ref_im.as_array()
     ## @EDO: Here you will need your rotation transformation and the BrainWeb image
-    cim = np.roll(cim, ind*5, axis=1) 
+
+    if True:
+        # The saved brainweb images are real, hence requires casting to complex
+        t1 = np.asarray( reg.NiftiImageData(T1_files[ind]).as_array(), dtype=np.complex64)
+        # The AcquisitionModel range has shape 1,256,256
+        # We plan to put in the brainweb data that are 150,150, so requires this requires padding
+        cim = np.pad(t1, ((0,256-150), (0,256-150)), 'constant', constant_values=0)
+        # and finally reshape to (1,256,256)
+        cim = np.reshape(cim, (1,256,256))
+    else:
+        cim = ref_im.as_array()
+        cim = np.roll(cim, ind*5, axis=1) 
+    print ("type", cim.dtype, cim.shape)
     ref_im_ms.fill(cim)
 
     acq_ms_sim[ind] = AcqMod.forward(ref_im_ms)
