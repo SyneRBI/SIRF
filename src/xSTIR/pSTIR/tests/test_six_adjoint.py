d683e426becf1d48539270e79a62db6f0621bb81
Ander Biguri <ander.biguri@gmail.com>
2020-04-23 12:13:21 +0100

    Make codacy happy

--- src/xSTIR/pSTIR/tests/test_six_adjoint.py
+++ src/xSTIR/pSTIR/tests/test_six_adjoint.py
@@ -13,7 +13,6 @@ Options:
 
 {licence}
 """
-import math
 import sirf.STIR as pet
 from sirf.Utilities import is_operator_adjoint, runner, __license__
 __version__ = "0.2.3"
@@ -21,23 +20,19 @@ __author__ = "Ander Biguri"
 
 def test_main(rec=False, verb=False, throw=True):
 
-    msg_red = pet.MessageRedirector()
+    pet.MessageRedirector()
     pet.set_verbosity(False)
     # create an acq_model that is explicitly a RayTracingMatrix
     am = pet.AcquisitionModelUsingRayTracingMatrix()
-    
     # load sample data
     data_path = pet.examples_data_path('PET')
     raw_data_file = pet.existing_filepath(data_path, 'Utahscat600k_ca_seg4.hs')
     ad = pet.AcquisitionData(raw_data_file)
-
-    # create sample image
+     # create sample image
     image = pet.ImageData()
     image.initialise(dim=(31, 111, 111), vsize=(2.25, 2.25, 2.25))
-
     # set up Acquisition Model
     am.set_up(ad,image)
-
     # test for adjointnesss
     if not is_operator_adjoint(am, verbose = verb):
       raise AssertionError('AcquisitionModelUsingRayTracingMatrix is not adjoint')
