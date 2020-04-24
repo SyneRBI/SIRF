845eb1c87c3fde737caaa2e4e1ca6b785ddd952e
richard <richard.brown@ucl.ac.uk>
2020-04-24 19:47:30 +0100

    do C++ python matlab

--- update_copyright_hook.sh
+++ update_copyright_hook.sh
@@ -1,17 +1,35 @@
 #!/bin/sh
 
-in1="^.*?This is software[.\s\S]+?\)\."
-out1="This is software developed originally for the Collaborative Computational
+# C
+in1="\nThis is software[.\s\S]+?\)\."
+out1="\nThis is software developed originally for the Collaborative Computational
 Project in Positron Emission Tomography and Magnetic Resonance Imaging
 (http://www.ccppetmr.ac.uk/) and now as part of CCP-SyneRBI."
+
+# Matlab
+in2="% This is software[.\s\S]+?\)\."
+out2="% This is software developed originally for the Collaborative Computational
+% Project in Positron Emission Tomography and Magnetic Resonance Imaging
+% (http://www.ccppetmr.ac.uk/) and now as part of CCP-SyneRBI."
+
+# Python
+in3="# This is software[.\s\S]+?\)\."
+out3="# This is software developed originally for the Collaborative Computational
+# Project in Positron Emission Tomography and Magnetic Resonance Imaging
+# (http://www.ccppetmr.ac.uk/) and now as part of CCP-SyneRBI."
 
 # Stash unstaged changes
 git stash -q --keep-index
 
 readonly FILES=$(git diff --diff-filter=ACMRTUXB --cached --name-only)
 
+ThereWereErrors=0
+
 for f in $FILES; do
-	perl -0777 -i -pe "s#${in1}#${out1}#g" $f
+	perl -0777 -i -pe "s&${in1}&${out1}&g" $f &&
+	perl -0777 -i -pe "s&${in2}&${out2}&g" $f &&
+	perl -0777 -i -pe "s&${in3}&${out3}&g" $f
+	if [ $? -ne 0 ]; then ThereWereErrors=1; fi
 done
 
 # Stage updated files
@@ -20,4 +38,4 @@ git add -u
 # Re-apply original unstaged changes
 git stash pop -q
 
-exit 0
+exit $ThereWereErrors
