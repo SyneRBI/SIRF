8b2c4e3107fdb6bcd4e4a224650d21257af6a991
richard <richard.brown@ucl.ac.uk>
2020-04-27 18:23:28 +0100

    rename NiftyPET_test and add documentation

--- tests/NiftyPET_test.sh
+++ tests/NiftyPET_test.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+function print_usage() {
+	echo "Usage: $0 [-h|--help] RAW_DATA_FOLDER TSTART TSTOP"
+}
+
+# Print full help
+if [[ $1 == "-h" || $1 == "--help" ]]; then
+	print_usage
+	echo -e "\nThis bash script compares the STIR-wrapped NiftyPET functionality to NiftyPET and, where possible, STIR.\n"
+	echo -e "### Test content\n"
+	echo -e "From raw dicom data (RAW_DATA_FOLDER) and a given time window (TSTART and TSTOP), prompt sinograms are extracted with NiftyPET, STIR-NiftyPET and STIR. Randoms are estimated, and for NiftyPET and STIR-NiftyPET, a norm sinogram is extracted.\n"
+	echo -e "Forward and back projections are performed with NiftyPET and STIR-NiftyPET, first with no corrections, then with norm, randoms and attenuation taken into account.\n"
+	echo -e "Lastly, a single MLEM iteration is performed with NiftyPET and STIR-NiftyPET.\n"
+	echo -e "At each step, the results are compared to ensure consistency.\n"
+	echo -e "At the time of writing, NiftyPET requires python2, so two different python executables should be set as environmental variables - NIFTYPET_PYTHON_EXECUTABLE and SIRF_PYTHON_EXECUTABLE.\n"
+	echo -e "### Data\n"
+	echo -e "An example of test data can be found here - [https://doi.org/10.5281/zenodo.1472951](https://doi.org/10.5281/zenodo.1472951).\n"
+	echo -e "Since the data needs to be read by NiftyPET, it should be in the raw form of .dcm/.bf, and not .l.hdr/.l.\n"
+	exit 0
+fi
+
 # Check input arguments
 if [[ "$#" -ne 3 ]]; then
-	echo "Usage: $0 RAW_DATA_FOLDER TSTART TSTOP"
+	print_usage
 	exit 1
 fi
 NTYP_RAW_DATA_FOLDER=$1
