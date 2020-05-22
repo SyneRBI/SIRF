f568ffa58af62c9f6976e116b816436027c525a7
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2020-05-22 08:52:03 +0000

    fixed #661

--- src/xSTIR/cSTIR/cstir.cpp
+++ src/xSTIR/cSTIR/cstir.cpp
@@ -726,7 +726,7 @@ void* cSTIR_setupReconstruction(void* ptr_r, void* ptr_i)
 			objectFromHandle<xSTIR_IterativeReconstruction3DF>(ptr_r);
 		Succeeded s = Succeeded::no;
 		if (!recon.post_process()) {
-			s = recon.setup(sptr_image);
+			s = recon.set_up(sptr_image);
 			recon.subiteration() = recon.get_start_subiteration_num();
 		}
 		if (s != Succeeded::yes) {
