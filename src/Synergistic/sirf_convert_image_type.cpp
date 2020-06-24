ac1dcb262301ba85fff8ffa135f09c01645d46c4
richard <richard.brown@ucl.ac.uk>
2020-06-24 16:26:06 +0100

    codacy performance changes

--- src/Synergistic/sirf_convert_image_type.cpp
+++ src/Synergistic/sirf_convert_image_type.cpp
@@ -112,7 +112,7 @@ void print_usage(const std::string &app_name)
 }
 
 /// throw error
-void err(const std::string message)
+void err(const std::string &message)
 {
     std::cerr << "\n" << message << "\n";
     exit(EXIT_FAILURE);
