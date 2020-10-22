e4e1369617148c41afafd75788d0f7a1926ec6fc
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2020-10-22 17:22:37 +0100

    added subsets options to PETAcquisitionModel norm computation

--- src/common/include/sirf/common/bf_operator.h
+++ src/common/include/sirf/common/bf_operator.h
@@ -6,13 +6,25 @@ namespace sirf {
 	class BFOperator : public anOperator<STIRImageData> {
 	public:
 		BFOperator(std::shared_ptr<PETAcquisitionModel> sptr_am) : sptr_am_(sptr_am) {}
+		void set_subset(int sub_num)
+		{
+			sub_num_ = sub_num;
+		}
+		void set_num_subsets(int num_sub)
+		{
+			num_sub_ = num_sub;
+		}
 		virtual std::shared_ptr<STIRImageData> apply(const STIRImageData& image_data) const
 		{
-			std::shared_ptr<PETAcquisitionData> sptr_fwd = sptr_am_->forward(image_data);
-			std::shared_ptr<STIRImageData> sptr_bwd = sptr_am_->backward(*sptr_fwd);
+			std::shared_ptr<PETAcquisitionData> sptr_fwd = 
+				sptr_am_->forward(image_data, sub_num_, num_sub_, true);
+			std::shared_ptr<STIRImageData> sptr_bwd = 
+				sptr_am_->backward(*sptr_fwd, sub_num_, num_sub_);
 			return sptr_bwd;
 		}
 	private:
 		std::shared_ptr<PETAcquisitionModel> sptr_am_;
+		int sub_num_ = 0;
+		int num_sub_ = 1;
 	};
 }
