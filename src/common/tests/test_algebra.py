62655752d4c51e4ef1caef4158b75fcf8b9348c2
Edoardo Pasca <edo.paskino@gmail.com>
2021-01-21 14:19:17 +0000

    added test algebra between datacontainers

--- src/common/tests/test_algebra.py
+++ src/common/tests/test_algebra.py
@@ -23,14 +23,12 @@ class TestSIRFDataContainerAlgebra(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.cwd)
 
-    def test_divide(self):
+    def test_divide_scalar(self):
         os.chdir(self.cwd)
         image1 = pet.ImageData('emission.hv')
         image2 = pet.ImageData('emission.hv')
         image1.fill(1.)
         image2.fill(2.)
-        print (image1.shape, image2.shape)
-        
         
         tmp = image1/1.
         numpy.testing.assert_array_equal(image1.as_array(), tmp.as_array())
@@ -41,14 +39,37 @@ class TestSIRFDataContainerAlgebra(unittest.TestCase):
         image1.divide(1., out=image2)
         numpy.testing.assert_array_equal(tmp.as_array(), image2.as_array())
         
+    def test_divide_datacontainer(self):
+        os.chdir(self.cwd)
+        # add 1 because the data contains zeros and divide is not going to be happy
+        image1 = pet.ImageData('emission.hv') + 1
+        image2 = pet.ImageData('emission.hv') + 1
+                
+        tmp = image1/image2
 
-    def test_multiply(self):
+        numpy.testing.assert_array_almost_equal(
+            numpy.ones(image1.shape, dtype=numpy.float32), tmp.as_array()
+            )
+    
+        tmp1 = image1.divide(image2)
+        numpy.testing.assert_array_almost_equal(
+            numpy.ones(image1.shape, dtype=numpy.float32), tmp1.as_array()
+            )
+        
+        tmp1.fill(2.)
+        image1.divide(image2, out=tmp1)
+        
+        numpy.testing.assert_array_almost_equal(
+            numpy.ones(image1.shape, dtype=numpy.float32), tmp1.as_array()
+            )
+        
+
+    def test_multiply_scalar(self):
         os.chdir(self.cwd)
         image1 = pet.ImageData('emission.hv')
         image2 = pet.ImageData('emission.hv')
         
         image2.fill(2.)
-        print (image1.shape, image2.shape)
         
         tmp = image1 * 1.
         numpy.testing.assert_array_equal(image1.as_array(), tmp.as_array())
@@ -59,14 +80,37 @@ class TestSIRFDataContainerAlgebra(unittest.TestCase):
         image1.multiply(1., out=image2)
         numpy.testing.assert_array_equal(tmp.as_array(), image2.as_array())
         
+    def test_multiply_datacontainer(self):
+        os.chdir(self.cwd)
+        # add 1 because the data contains zeros and divide is not going to be happy
+        image1 = pet.ImageData('emission.hv')
+        image2 = pet.ImageData('emission.hv')
+        
+        image2.fill(1.)
+        tmp = image1 * image2
+
+        numpy.testing.assert_array_almost_equal(
+            image1.as_array(), tmp.as_array()
+            )
     
-    def test_add(self):
+        tmp1 = image1.multiply(image2)
+        numpy.testing.assert_array_almost_equal(
+            image1.as_array(), tmp1.as_array()
+            )
+        
+        tmp1.fill(2.)
+        image1.multiply(image2, out=tmp1)
+        
+        numpy.testing.assert_array_almost_equal(
+            image1.as_array(), tmp1.as_array()
+            )
+        
+    def test_add_scalar(self):
         os.chdir(self.cwd)
         image1 = pet.ImageData('emission.hv')
         image2 = pet.ImageData('emission.hv')
         image1.fill(0)
         image2.fill(1)
-        print (image1.shape, image2.shape)
         
         tmp = image1 + 1.
         numpy.testing.assert_array_equal(image2.as_array(), tmp.as_array())
@@ -78,13 +122,39 @@ class TestSIRFDataContainerAlgebra(unittest.TestCase):
         image1.add(1., out=tmp1)
         numpy.testing.assert_array_equal(tmp1.as_array(), image2.as_array())
     
-    def test_subtract(self):
+    def test_add_datacontainer(self):
+        os.chdir(self.cwd)
+        # add 1 because the data contains zeros and divide is not going to be happy
+        image1 = pet.ImageData('emission.hv')
+        image2 = pet.ImageData('emission.hv')
+        image1.fill(0.)
+        image2.fill(1.)
+        tmp = image1 + image2
+
+        numpy.testing.assert_array_almost_equal(
+            numpy.ones(image1.shape, dtype=numpy.float32), tmp.as_array()
+            )
+    
+        tmp1 = image1.add(image2)
+        
+        numpy.testing.assert_array_almost_equal(
+            numpy.ones(image1.shape, dtype=numpy.float32), tmp1.as_array()
+            )
+        
+        tmp1.fill(2.)
+        image1.add(image2, out=tmp1)
+        
+        numpy.testing.assert_array_almost_equal(
+            numpy.ones(image1.shape, dtype=numpy.float32), tmp1.as_array()
+            )
+        
+    
+    def test_subtract_scalar(self):
         os.chdir(self.cwd)
         image1 = pet.ImageData('emission.hv')
         image2 = pet.ImageData('emission.hv')
         image1.fill(2)
         image2.fill(1)
-        print (image1.shape, image2.shape)
         
         tmp = image1 - 1.
         numpy.testing.assert_array_equal(image2.as_array(), tmp.as_array())
@@ -95,3 +165,28 @@ class TestSIRFDataContainerAlgebra(unittest.TestCase):
         tmp1.fill(0)
         image1.subtract(1., out=tmp1)
         numpy.testing.assert_array_equal(tmp1.as_array(), image2.as_array())
+    
+    def test_subtract_datacontainer(self):
+        os.chdir(self.cwd)
+        # add 1 because the data contains zeros and divide is not going to be happy
+        image1 = pet.ImageData('emission.hv')
+        image2 = pet.ImageData('emission.hv')
+        
+        tmp = image1 - image2
+
+        numpy.testing.assert_array_almost_equal(
+            numpy.zeros(image1.shape, dtype=numpy.float32), tmp.as_array()
+            )
+    
+        tmp1 = image1.subtract(image2)
+        
+        numpy.testing.assert_array_almost_equal(
+            numpy.zeros(image1.shape, dtype=numpy.float32), tmp1.as_array()
+            )
+        
+        tmp1.fill(2.)
+        image1.subtract(image2, out=tmp1)
+        
+        numpy.testing.assert_array_almost_equal(
+            numpy.zeros(image1.shape, dtype=numpy.float32), tmp1.as_array()
+            )
