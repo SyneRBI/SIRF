5367357e2fa3bbed4c4a356beaa2855d94485fd9
johannesmayer <johannes.mayer@ptb.de>
2021-06-02 16:00:13 +0000

    Modified steepest descent example to start from quasi-zero-filled image

--- examples/Python/MR/grappa_and_steepest_descent.py
+++ examples/Python/MR/grappa_and_steepest_descent.py
@@ -106,6 +106,9 @@ def main():
     # image_data
     acq_model = AcquisitionModel(preprocessed_data, image_data)
     acq_model.set_coil_sensitivity_maps(csms)
+    
+    start_val = 0.001
+    image_data = image_data.fill(start_val + start_val * 1j)
 
     res = numpy.ndarray((niter,))
     scale = 1 # reconstructed image and phantom may have different scale,
