39bbf5402dbbdf27b133b76cd5b692d9512a10b9
johannesmayer <johannes.mayer@ptb.de>
2021-07-26 14:31:47 +0000

    Modified MR dicom python test

--- src/xGadgetron/pGadgetron/tests/tests_mrimage_geometry.py
+++ src/xGadgetron/pGadgetron/tests/tests_mrimage_geometry.py
@@ -16,13 +16,12 @@ Options:
 {licence}
 """
 
-import sirf.Reg as sreg
 from sirf.Gadgetron import *
-from sirf.Utilities import runner, RE_PYEXT, __license__
+from sirf.Utilities import runner, __license__
 __version__ = "0.2.3"
 __author__ = "Johannes Mayer"
 
-def test_2d_slice_stack(rec=False, verb=False, throw=True):
+def test_MR_DICOM_writer(rec=False, verb=False, throw=True):
 
     print("Running the img geometry test")
     data_path = examples_data_path('MR')
@@ -30,31 +29,28 @@ def test_2d_slice_stack(rec=False, verb=False, throw=True):
 
     rawdata = preprocess_acquisition_data(rawdata)
     rawdata.sort()
-    
-    imgdata = ImageData()
-    imgdata.from_acquisition_data(rawdata)
-    
-    print("The image positions are {}".format(imgdata.get_ISMRMRD_info('position')))
-
-    print("The rawdata directions are: {}".format(rawdata.get_ISMRMRD_info('read_dir')[0]))
-    print("The rawdata directions are: {}".format(rawdata.get_ISMRMRD_info('phase_dir')[0]))
-    print("The rawdata directions are: {}".format(rawdata.get_ISMRMRD_info('slice_dir')[0]))
-
-    print("The image data directions are: {}".format(imgdata.get_ISMRMRD_info('read_dir')[0]))
-    print("The image data directions are: {}".format(imgdata.get_ISMRMRD_info('phase_dir')[0]))
-    print("The image data directions are: {}".format(imgdata.get_ISMRMRD_info('slice_dir')[0]))
-
+   
     recon = FullySampledReconstructor()
     recon.set_input(rawdata)
     recon.process()
     img_data = recon.get_output()
     
+    print("The image positions are {}".format(img_data.get_ISMRMRD_info('position')))
+
+    print("The rawdata directions are: {}".format(rawdata.get_ISMRMRD_info('read_dir')[0]))
+    print("The rawdata directions are: {}".format(rawdata.get_ISMRMRD_info('phase_dir')[0]))
+    print("The rawdata directions are: {}".format(rawdata.get_ISMRMRD_info('slice_dir')[0]))
+
+    print("The image data directions are: {}".format(img_data.get_ISMRMRD_info('read_dir')[0]))
+    print("The image data directions are: {}".format(img_data.get_ISMRMRD_info('phase_dir')[0]))
+    print("The image data directions are: {}".format(img_data.get_ISMRMRD_info('slice_dir')[0]))
+
     img_data = img_data.abs()
-    img_data.write( data_path+ "/tmp_mrgeometry.dcm")
+    img_data.write( data_path+ "/output_test_MR_DICOM_writer.dcm")
     
     test_failed = False
     return test_failed, 1
 
 
 if __name__ == "__main__":
-    runner(test_2d_slice_stack, __doc__, __version__, __author__)
+    runner(test_MR_DICOM_writer, __doc__, __version__, __author__)
