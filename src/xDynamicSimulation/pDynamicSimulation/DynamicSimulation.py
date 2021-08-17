d718b5238c2a2f744f8d05f42c60cf9401ece988
johannesmayer <johannes.mayer@ptb.de>
2021-08-17 15:00:15 +0000

    fixed type for setting up surrogate signal

--- src/xDynamicSimulation/pDynamicSimulation/DynamicSimulation.py
+++ src/xDynamicSimulation/pDynamicSimulation/DynamicSimulation.py
@@ -41,7 +41,7 @@ else:
 
 from deprecation import deprecated
 
-from sirf.Utilities import check_status, assert_validity
+from sirf.Utilities import check_status, assert_validity, try_calling, error 
 
 import sirf
 from sirf import SIRF
@@ -93,13 +93,38 @@ class DynamicSimulation(object):
 class SurrogateSignal(object):
     def __init__(self, time_points, signal_points):
 
+        if not isinstance(time_points, np.ndarray):
+            raise error('Wrong input format.' + \
+                ' Should be numpy.ndarray. Got {}'.format(type(time_points)))
+
+        if not isinstance(signal_points, np.ndarray):
+            raise error('Wrong input format.' + \
+                ' Should be numpy.ndarray. Got {}'.format(type(signal_points)))
+
         num_signal_points = time_points.size
         if num_signal_points != signal_points.size:
             AssertionError("The signal and time points do not have the identical size.")
 
+        if time_points.dtype != np.float32:
+            the_time = time_points.astype(np.float32)
+        
+        if signal_points.dtype != np.float32:
+            the_signal = signal_points.astype(np.float32)
+
+        convert = not the_time.flags['C_CONTIGUOUS']
+        if convert:
+            the_time = np.ascontiguousarray(the_time)
+
+        convert = not the_signal.flags['C_CONTIGUOUS']
+        if convert:
+            the_signal = np.ascontiguousarray(the_signal)
+        
         self.handle = None
-        self.handle = pysim.cDS_DynamicSignal(time_points.ctypes.data, signal_points.ctypes.data, num_signal_points)
+        self.handle = pysim.cDS_DynamicSignal(the_time.ctypes.data,\
+                                    the_signal.ctypes.data,\
+                                    num_signal_points)
         check_status(self.handle)
+
 
 class Dynamic(object):
 
