eb7c9c91b2b05a16f309510520f7b188bf2f3591
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2021-10-29 12:58:27 +0100

    added doxygen comments to newly added conjugation methods of DataContainer

--- src/common/include/sirf/common/DataContainer.h
+++ src/common/include/sirf/common/DataContainer.h
@@ -88,20 +88,26 @@ namespace sirf {
 		{
 			return items() < 1;
 		}
+
 		std::unique_ptr<DataContainer> clone() const
 		{
 			return std::unique_ptr<DataContainer>(this->clone_impl());
 		}
+
+		/// overwrites this container's complex data with complex conjugate values
 		void conjugate()
 		{
 			this->conjugate_impl();
 		}
+
+		///  returns unique pointer to the complex-conjugated copy of this container
 		std::unique_ptr<DataContainer> conjugate() const
 		{
 			DataContainer* ptr = this->clone_impl();
 			ptr->conjugate();
 			return std::unique_ptr<DataContainer>(ptr);
 		}
+
 	protected:
 		virtual DataContainer* clone_impl() const = 0;
 		virtual void conjugate_impl()
