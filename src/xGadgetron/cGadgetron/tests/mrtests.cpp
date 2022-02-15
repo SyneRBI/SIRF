d06de2c5a4b13442ada6bbf0fed08e5f412b6845
johannesmayer <johannes.mayer1991@gmail.com>
2022-02-15 13:55:32 +0000

    Added tests for setting trajectory and type

--- src/xGadgetron/cGadgetron/tests/mrtests.cpp
+++ src/xGadgetron/cGadgetron/tests/mrtests.cpp
@@ -96,6 +96,54 @@ bool test_get_subset(const MRAcquisitionData& av)
     }
 }
 
+bool test_set_trajectory_type(const MRAcquisitionData& ad)
+{
+    try{
+        std::cout << "Running " << __FUNCTION << std::endl;
+        auto sptr_av = ad.clone();
+        std::cout << "Our trajectory type is : " << static_cast<int>(sptr_av->get_trajectory_type()) << std::endl;
+        sptr_av ->set_trajectory_type(ISMRMRD::TrajectoryType::SPIRAL);
+        std::cout << "Our trajectory type is : " << static_cast<int>(sptr_av->get_trajectory_type()) << std::endl;
+    }
+    catch( std::runtime_error const &e)
+    {
+        std::cout << "Exception caught " <<__FUNCTION__ <<" .!" <<std::endl;
+        std::cout << e.what() << std::endl;
+        throw;
+    }
+}
+
+bool test_set_trajectory(const MRAcquisitionData& ad)
+{
+    try
+    {
+        auto sptr_av = ad.clone();
+
+        int const num_readouts = sptr_av->number();
+        
+        std::vector<int> dims{0,0,0};
+        sptr_av->get_acquisitions_dimensions((size_t)&dims[0]);
+        int const num_samples = (int)dims[0];
+
+        int const traj_dim = 2;
+
+        size_t const num_trajectory_pts = traj_dim*num_samples*num_readouts;
+        std::cout << "Our container has " << num_readouts << " readouts with " << num_samples << " samples." << std::endl;
+        std::vector<float> traj(num_trajectory_pts);
+
+        sptr_av->set_trajectory(traj_dim,&traj[0]);
+
+        return true;
+
+    }
+    catch( std::runtime_error const &e)
+    {
+        std::cout << "Exception caught " <<__FUNCTION__ <<" .!" <<std::endl;
+        std::cout << e.what() << std::endl;
+        throw;
+    }
+}
+
 bool test_ISMRMRDImageData_from_MRAcquisitionData(MRAcquisitionData& av)
 {
      try
@@ -652,6 +700,8 @@ bool run_cartesian_tests(const std::string& filename_testdata)
 
     ok *= test_get_kspace_order(av);
     ok *= test_get_subset(av);
+    ok *= test_set_trajectory_type(av);
+    ok *= test_set_trajectory(av);
 
     ok *= test_ISMRMRDImageData_from_MRAcquisitionData(av);
     ok *= test_ISMRMRDImageData_reorienting(av);
