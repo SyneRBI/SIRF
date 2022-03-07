7ac55d36c38b80fd3135b9e493ea75351d469ff6
samdporter <92305641+samdporter@users.noreply.github.com>
2022-03-07 17:25:07 +0000

    Update basic_reconstruction.py
    Minor edits in preamble

--- examples/Python/SPECT/interactive/basic_reconstruction.py
+++ examples/Python/SPECT/interactive/basic_reconstruction.py
@@ -14,11 +14,11 @@
 #
 ## CCP SyneRBI Synergistic Image Reconstruction Framework (SIRF)
 ## Copyright 2015 - 2017 Rutherford Appleton Laboratory STFC
-## Copyright 2015 - 2017, 2019 University College London.
+## Copyright 2015 - 2022 University College London.
 ##
 ## This is software developed for the Collaborative Computational
 ## Project in Synergistic Reconstruction for Biomedical Imaging
-## (http://www.ccppetmr.ac.uk/).
+## (http://www.ccpsynerbi.ac.uk/).
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ##   you may not use this file except in compliance with the License.
