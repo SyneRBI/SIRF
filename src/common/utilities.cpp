6e242eccb2e72aa8803a728ad1431ece7970ae3b
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2023-01-06 17:34:34 +0000

    removed unused fix_path_separator and append_path

--- src/common/utilities.cpp
+++ src/common/utilities.cpp
@@ -13,19 +13,19 @@ namespace sirf {
 			return '/';
 		return filename[found];
 	}
-	std::string append_path(const std::string& path, int na, const char* a, ...)
-	{
-		va_list args;
-		va_start(args, na);
-		std::string r = path;
-		for (int i = 0; i < na; i++) {
-			const char* s = va_arg(args, const char*);
-			r += path_separator();
-			r += s;
-		}
-		va_end(args);
-		return r;
-	}
+	//std::string append_path(const std::string& path, int na, const char* a, ...)
+	//{
+	//	va_list args;
+	//	va_start(args, na);
+	//	std::string r = path;
+	//	for (int i = 0; i < na; i++) {
+	//		const char* s = va_arg(args, const char*);
+	//		r += path_separator();
+	//		r += s;
+	//	}
+	//	va_end(args);
+	//	return r;
+	//}
 	std::string append_path(std::string path, const char* a, ...)
 	{
 		std::string r = path;
