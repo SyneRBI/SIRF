7d45b876de39bbe645f69917cb40fcea19f7bd70
johannesmayer <johannes.mayer1991@gmail.com>
2023-03-22 10:31:51 +0000

    Removed declaraations for CS and acuqiistion parameter setters in glue code

--- src/xGadgetron/cGadgetron/include/sirf/Gadgetron/cgadgetron.h
+++ src/xGadgetron/cGadgetron/include/sirf/Gadgetron/cgadgetron.h
@@ -58,8 +58,6 @@ extern "C" {
 	void* cGT_setAcquisitionModelParameter
 		(void* ptr_am, const char* name, const void* ptr);
 	void* cGT_AcquisitionModelParameter(void* ptr_am, const char* name);
-	void* cGT_setCSParameter(void* ptr, const char* par, const void* val);
-	void* cGT_setAcquisitionParameter(void* ptr, const char* param_name, const void* val);
 	void* cGT_setCSMs(void* ptr_am, const void* ptr_csms);
 	void* cGT_acquisitionModelNorm(void* ptr_am, int num_iter, int verb);
 	void* cGT_AcquisitionModelForward(void* ptr_am, const void* ptr_imgs);
