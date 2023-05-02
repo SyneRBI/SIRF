06930af6ae3768fdedadc626040102220008bef0
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2023-05-02 14:39:08 +0100

    applied algebra tests in Uitliies.py to NiftiImageData containers

---
+++ src/Registration/pReg/tests/tests_data_container_algebra.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""sirf.STIR algebra tests
+v{version}
+
+Usage:
+  tests_six [--help | options]
+
+Options:
+  -r, --record   not used
+  -v, --verbose  report each test status
+
+{author}
+
+{licence}
+"""
+import os
+from sirf.Utilities import runner, pTest, RE_PYEXT, __license__
+from sirf.Utilities import examples_data_path
+from sirf.Utilities import test_data_container_algebra
+from sirf.Reg import MessageRedirector
+import sirf.Reg as reg
+
+__version__ = "0.2.3"
+__author__ = "Evgueni Ovtchinnikov"
+
+
+def test_main(rec=False, verb=False, throw=True):
+    #MessageRedirector()
+
+    datafile = RE_PYEXT.sub(".txt", __file__)
+    test = pTest(datafile, rec, throw=throw)
+    test.verbose = verb
+    data_path = examples_data_path('Registration')
+    image = reg.ImageData(os.path.join(data_path, 'test2.nii.gz'))
+    image /= image.norm()
+    test_data_container_algebra(test, image)
+
+    return test.failed, test.ntest
+
+
+if __name__ == "__main__":
+    runner(test_main, __doc__, __version__, __author__)
