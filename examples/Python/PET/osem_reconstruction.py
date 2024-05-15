480787186f63107f817112a9acfe0a0ddcc7cd69
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2024-05-15 16:56:08 +0000

    attended to Codacy issues

--- examples/Python/PET/osem_reconstruction.py
+++ examples/Python/PET/osem_reconstruction.py
@@ -128,7 +128,7 @@ def main():
     print('setting up, please wait...')
     recon.set_up(image)
 
-    q = obj_fun.test_Hessian(image, subset=0)
+    obj_fun.test_Hessian(image, subset=0)
 
     # set the initial image estimate
     recon.set_current_estimate(image)
