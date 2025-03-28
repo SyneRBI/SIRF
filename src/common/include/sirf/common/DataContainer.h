757c866410618ca1c75ce1cd2d727a96960512c5
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2025-03-28 18:51:01 +0000

    removed commented-out old approach lines from DataContainer.h

--- src/common/include/sirf/common/DataContainer.h
+++ src/common/include/sirf/common/DataContainer.h
@@ -55,14 +55,7 @@ A class for a set of generally heterogeneous items of data.
 Has vector features: norm, dot product, linear combination,
 which rely on the same features of the items.
 */
-//<<<<<<< HEAD
-
-//namespace sirf {
-
-//	class DataContainer {
-//=======
 	class DataContainer: public virtual ContainerBase {
-//>>>>>>> master
 	public:
 		virtual ~DataContainer() {}
 
@@ -85,102 +78,7 @@ which rely on the same features of the items.
 			return is_complex() ? 16 * sizeof(float) : 8 * sizeof(float);
 		}
 
-//<<<<<<< HEAD
 		virtual void write(const std::string& filename) const = 0;
-/*
-=======
-		/// returns the norm of this container viewed as a vector
-		virtual float norm() const = 0;
-
-		/// below all void* are actually either float* (STIR containers and NiftiImageData)
-		/// or complex_float_t* (Gadgetron containers)
-
-		/// calculates the dot product of this container with another one
-		virtual void dot(const DataContainer& dc, void* ptr) const = 0;
-
-		/// calculates the sum of this container elements
-		virtual void sum(void* ptr) const = 0;
-
-		/// calculates the value of this container's element with the largest real part
-		virtual void max(void* ptr) const = 0;
-
-		/// calculates the value of this container's element with the smallest real part
-		virtual void min(void* ptr) const = 0;
-
-		/// \c *this = the elementwise product \c x*y
-		virtual void multiply
-			(const DataContainer& x, const DataContainer& y) = 0;
-		/// \c *this = the product \c x * y with scalar y
-		virtual void multiply
-			(const DataContainer& x, const void* ptr_y) = 0;
-
-		/// \c *this = the sum \c x + y with scalar y
-		virtual void add
-			(const DataContainer& x, const void* ptr_y) = 0;
-
-		/// \c *this = the elementwise ratio \c x / y
-		virtual void divide
-			(const DataContainer& x, const DataContainer& y) = 0;
-
-		/// \c *this = the elementwise \c max(x, y)
-		virtual void maximum
-			(const DataContainer& x, const DataContainer& y) = 0;
-		virtual void maximum
-			(const DataContainer& x, const void* ptr_y) = 0;
-
-		/// \c *this = the elementwise \c min(x, y)
-		virtual void minimum
-			(const DataContainer& x, const DataContainer& y) = 0;
-		virtual void minimum
-			(const DataContainer& x, const void* ptr_y) = 0;
-
-		/// \c *this = the elementwise \c pow(x, y)
-		virtual void power
-			(const DataContainer& x, const DataContainer& y) = 0;
-		virtual void power
-			(const DataContainer& x, const void* ptr_y) = 0;
-
-		/// \c *this = the elementwise \c exp(x)
-		virtual void exp(const DataContainer& x) = 0;
-		/// \c *this = the elementwise \c log(x)
-		virtual void log(const DataContainer& x) = 0;
-		/// \c *this = the elementwise \c sqrt(x)
-		virtual void sqrt(const DataContainer& x) = 0;
-		/// \c *this = the elementwise \c sign(x)
-		virtual void sign(const DataContainer& x) = 0;
-		/// \c *this = the elementwise \c abs(x)
-		virtual void abs(const DataContainer& x) = 0;
-
-		/// \c *this = the linear combination of \c x and \c y
-		virtual void axpby(
-			const void* ptr_a, const DataContainer& x,
-			const void* ptr_b, const DataContainer& y) = 0;
-		/// alternative interface to the above
-		virtual void xapyb(
-			const DataContainer& x, const void* ptr_a,
-			const DataContainer& y, const void* ptr_b) = 0;
-
-		/// \c *this = elementwise sum of two elementwise products \c x*a and \c y*b
-		virtual void xapyb(
-			const DataContainer& x, const DataContainer& a,
-			const DataContainer& y, const DataContainer& b) = 0;
-
-		/// \c *this = elementwise sum of \c x*a and elementwise \c y*b
-		virtual void xapyb(
-			const DataContainer& a_x, const void* ptr_a,
-			const DataContainer& a_y, const DataContainer& a_b) = 0;
-
-		/// \c *this = elementwise sum of elementwise \c x*a and \c y*b
-		void xapyb(
-			const DataContainer& a_x, const DataContainer& a_a,
-			const DataContainer& a_y, const void* ptr_b)
-		{
-			xapyb(a_y, ptr_b, a_x, a_a);
-		}
-
-		virtual void write(const std::string &filename) const = 0;
->>>>>>> master
-*/
 
 		bool is_empty() const
 		{
@@ -331,27 +229,14 @@ which rely on the same features of the items.
 		virtual void xapyb(
 			const DataContainer& x, T a,
 			const DataContainer& y, T b) = 0;
-		//virtual void axpby(
-		//	const void* ptr_a, const DataContainer& x,
-		//	const void* ptr_b, const DataContainer& y) = 0;
-		/// alternative interface to the above
-		//virtual void xapyb(
-		//	const DataContainer& x, const void* ptr_a,
-		//	const DataContainer& y, const void* ptr_b) = 0;
-
 		/// \c *this = elementwise sum of two elementwise products \c x*a and \c y*b
 		virtual void xapyb(
 			const DataContainer& x, const DataContainer& a,
 			const DataContainer& y, const DataContainer& b) = 0;
-
 		/// \c *this = elementwise sum of \c x*a and elementwise \c y*b
 		virtual void xapyb(
 			const DataContainer& a_x, T a,
 			const DataContainer& a_y, const DataContainer& a_b) = 0;
-		//virtual void xapyb(
-		//	const DataContainer& a_x, const void* ptr_a,
-		//	const DataContainer& a_y, const DataContainer& a_b) = 0;
-
 		/// \c *this = elementwise sum of elementwise \c x*a and \c y*b
 		void xapyb(
 			const DataContainer& a_x, const DataContainer& a_a,
@@ -359,12 +244,6 @@ which rely on the same features of the items.
 		{
 			xapyb(a_y, b, a_x, a_a);
 		}
-		//void xapyb(
-		//	const DataContainer& a_x, const DataContainer& a_a,
-		//	const DataContainer& a_y, const void* ptr_b)
-		//{
-		//	xapyb(a_y, ptr_b, a_x, a_a);
-		//}
 
 	protected:
 
