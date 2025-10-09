ae475dcccf744c5af73c73f797ad84e8aebdb936
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2025-10-09 17:55:50 +0000

    added gadgetron data views failure example

--- examples/Python/MR/data_views.py
+++ examples/Python/MR/data_views.py
@@ -5,11 +5,7 @@ v{version}
 DataContainer algebra tests
 
 Usage:
-  data_views [--help | options]
-
-Options:
-  -r, --record   record the measurements rather than check them
-  -v, --verbose  report each test status
+  data_views.py
 
 {author}
 
@@ -18,17 +14,13 @@ Options:
 from sirf.SIRF import norm, dot, copyto
 from sirf.Gadgetron import AcquisitionData, \
     AcquisitionDataView, ImageDataView, FullySampledReconstructor
-from sirf.Utilities import pTest, runner, RE_PYEXT, examples_data_path
+from sirf.Utilities import pTest, runner, RE_PYEXT, examples_data_path, error
 import numpy
 __version__ = "3.1.0"
 __author__ = "Evgueni Ovtchinnikov, Casper da Costa-Luis"
 
 
-def test_main(rec=False, verb=False, throw=True, no_ret_val=True):
-    datafile = RE_PYEXT.sub(".txt", __file__)
-    test = pTest(datafile, rec, throw=throw)
-    test.verbose = verb
-
+def main():
     data_path = examples_data_path('MR')
     AcquisitionData.set_storage_scheme('memory')
     input_data = AcquisitionData(data_path + '/simulated_MR_2D_cartesian.h5')
@@ -39,6 +31,11 @@ def test_main(rec=False, verb=False, throw=True, no_ret_val=True):
     y = x + 0
     z = x + 0
 
+    acq = y.acquisition(0)
+    print(type(acq))
+    print(acq.number_of_samples())
+    acq_view = acq.asarray(copy=False)
+
     x_view = AcquisitionDataView(x)
     y_view = AcquisitionDataView(y)
     z_view = AcquisitionDataView(z)
@@ -48,22 +45,22 @@ def test_main(rec=False, verb=False, throw=True, no_ret_val=True):
     copyto(y_view, x_view)
     y_view *= 2
     norm_y_view = norm(y_view)
-    test.check_if_equal_within_tolerance(norm_y, norm_y_view, rel_tol=1e-5)
+#    test.check_if_equal_within_tolerance(norm_y, norm_y_view, rel_tol=1e-5)
     z = x + y
     norm_z = z.norm()
     copyto(z_view, x_view)
     z_view += y_view
     norm_z_view = norm(z_view)
-    test.check_if_equal_within_tolerance(norm_z, norm_z_view, rel_tol=1e-5)
+#    test.check_if_equal_within_tolerance(norm_z, norm_z_view, rel_tol=1e-5)
     s = norm(x)
     t = norm(x_view)
-    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
+#    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
     s = x.sum()
     t = x_view.sum()
-    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
+#    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
     s = x.dot(y)
     t = dot(x_view, y_view)
-    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
+#    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
 
     recon = FullySampledReconstructor()
     recon.set_input(processed_data)
@@ -83,28 +80,36 @@ def test_main(rec=False, verb=False, throw=True, no_ret_val=True):
     copyto(y_view, x_view)
     y_view *= 2
     norm_y_view = norm(y_view)
-    test.check_if_equal_within_tolerance(norm_y, norm_y_view, rel_tol=1e-5)
+#    test.check_if_equal_within_tolerance(norm_y, norm_y_view, rel_tol=1e-5)
     z = x + y
     norm_z = z.norm()
     copyto(z_view, x_view)
     z_view += y_view
     norm_z_view = norm(z_view)
-    test.check_if_equal_within_tolerance(norm_z, norm_z_view, rel_tol=1e-5)
+#    test.check_if_equal_within_tolerance(norm_z, norm_z_view, rel_tol=1e-5)
     s = norm(x)
     t = norm(x_view)
-    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
+#    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
     s = x.sum()
     t = x_view.sum()
-    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
+#    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
     s = x.dot(y)
     t = dot(x_view, y_view)
-    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
+#    test.check_if_equal_within_tolerance(s, t, rel_tol=1e-5)
 
-    numpy.testing.assert_equal(test.failed, 0)
-    if no_ret_val:
-        return
-    return test.failed, test.ntest
+#    numpy.testing.assert_equal(test.failed, 0)
+#    if no_ret_val:
+#        return
+#    return test.failed, test.ntest
 
+try:
+    main()
+    print('\n=== done with %s' % __file__)
 
-if __name__ == "__main__":
-    runner(test_main, __doc__, __version__, __author__, no_ret_val=False)
+except error as err:
+    # display error information
+    print('??? %s' % err.value)
+    exit(1)
+
+#if __name__ == "__main__":
+#    runner(test_main, __doc__, __version__, __author__, no_ret_val=False)
