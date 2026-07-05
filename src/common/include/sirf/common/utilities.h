fb85a60f6b8686bee904894752456557112d9aeb
Kris Thielemans@ucl.ac.uk <k.thielemans@ucl.ac.uk>
2026-07-05 10:42:30 +0100

    move pointer_supports_cuda_array_view impl to .cxx

--- src/common/include/sirf/common/utilities.h
+++ src/common/include/sirf/common/utilities.h
@@ -31,11 +31,6 @@ limitations under the License.
 
 #include <string>
 
-#ifdef HAS_CUDA_RUNTIME_API
-// cppcheck-suppress missingIncludeSystem
-#include <cuda_runtime_api.h>
-#endif
-
 namespace sirf {
     //! return the path-separator used by the OS
     /*!
@@ -66,7 +61,6 @@ namespace sirf {
     {
       return append_path(append_path(path, a), args...);
     }
-    ///@{
     //! Returns where the examples are installed
     /*!
       \ingroup Common
@@ -77,26 +71,9 @@ namespace sirf {
     */
     std::string examples_data_path(const char* data_type);
 
-    inline bool
-    pointer_supports_cuda_array_view(const void* ptr)
-    {
-#ifdef HAS_CUDA_RUNTIME_API
-        if (ptr == nullptr)
-            return false;
-
-        cudaPointerAttributes attrs{};
-        const cudaError_t err = cudaPointerGetAttributes(&attrs, ptr);
-        if (err != cudaSuccess)
-            return false;
-#if CUDART_VERSION >= 10000
-        return attrs.type == cudaMemoryTypeManaged;
-#else
-        return attrs.memoryType == cudaMemoryTypeManaged && attrs.isManaged;
-#endif
-#else
-        return false;
-#endif
-    }
+    //! Returns if the pointer is a CUDA pointer or CUDA managed pointer
+    bool
+    pointer_supports_cuda_array_view(const void* ptr);
 }
 
 #endif
