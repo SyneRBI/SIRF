8d6ed5af56630dc1fcfa3fd910ea94edfb1d9817
Casper da Costa-Luis <casper.dcl@physics.org>
2026-07-21 19:36:44 +0000

    pytest Synergistic

--- src/Synergistic/pSyn/tests/tests_syn.py
+++ src/Synergistic/pSyn/tests/tests_syn.py
@@ -1,13 +1,15 @@
-import sys
-
-from sirf.SIRF import ImageData
+import pytest
 from sirf.Reg import NiftiImageData3D
+from sirf.SIRF import ImageData
+from sirf.Utilities import examples_data_path, existing_filepath
 
-na = len(sys.argv)
-if na < 3:
-    sys.exit(1)
 
-image = ImageData()
-image.read(sys.argv[1], sys.argv[2], 1)
-nifti_image = NiftiImageData3D(image)
-sys.exit(image != nifti_image)
+@pytest.mark.parametrize("engine", ["Gadgetron", "STIR"])
+def test_nifti_image_data_3d(engine):
+    filename = {"Gadgetron": "SIRF_recon.h5", "STIR": "dicom_as_nifti.nii"}[engine]
+    data_path = examples_data_path('MR')
+    raw_data_file = existing_filepath(f"{data_path}/zenodo", filename)
+    image = ImageData()
+    image.read(raw_data_file, engine, 1)
+    nifti_image = NiftiImageData3D(image)
+    assert image == nifti_image
