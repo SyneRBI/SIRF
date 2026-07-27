8b821a2ec0e716d4a4aa542162406891961b3552
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2026-07-27 13:46:11 +0000

    fixed division by zero in tests_data_container_algebra

--- src/Registration/pReg/tests/tests_data_container_algebra.py
+++ src/Registration/pReg/tests/tests_data_container_algebra.py
@@ -34,7 +34,8 @@ def test_main(rec=False, verb=False, throw=True, no_ret_val=True):
     data_path = examples_data_path('Registration')
     image = reg.ImageData(os.path.join(data_path, 'test2.nii.gz'))
     image /= image.norm()
-    data_container_algebra_tests(test, image) # TODO: `image` should be non-zero as it's used as a divisor
+    image += image.min() + 1e-6
+    data_container_algebra_tests(test, image)
 
     numpy.testing.assert_equal(test.failed, 0)
     if no_ret_val:
--- src/xSTIR/pSTIR/tests/tests_data_container_algebra.py
+++ src/xSTIR/pSTIR/tests/tests_data_container_algebra.py
@@ -34,7 +34,11 @@ def test_main(rec=False, verb=False, throw=True, no_ret_val=True):
         AcquisitionData.set_storage_scheme(scheme)
         raw_data_file = existing_filepath(data_path, 'Utahscat600k_ca_seg4.hs')
         ad = AcquisitionData(raw_data_file)
-        data_container_algebra_tests(test, ad) # TODO: ad should be non-zero as it's used as a divisor
+        ad_arr = ad.as_array()
+        ad_arr += ad_arr.min() + 1e-6
+        adc = ad.clone()
+        adc.fill(ad_arr)
+        data_container_algebra_tests(test, adc)
 
         image_size = (31, 111, 111)
         voxel_size = (3.375, 3, 3)
